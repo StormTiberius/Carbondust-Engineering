3b78c1bfd3b97d34ead842d0608b75405486963f
StormTiberius <equestrian@equestria.net>
2018-02-22 21:53:22 +0200

    DM

--- cde/ember/DungeonManager.java
+++ cde/ember/DungeonManager.java
@@ -5,36 +5,25 @@
 
 package cde.ember;
 
-import cde.ember.dungeons.VillageDungeon;
-import cde.ember.dungeons.VanillaDungeon;
-import cde.ember.dungeons.JungleDungeon;
 import cde.ember.dungeons.StarterDungeon;
-import cde.ember.dungeons.NetherDungeon;
-import cde.ember.dungeons.DesertDungeon;
+import cde.ember.dungeons.VanillaDungeon;
 import java.util.Random;
+import net.minecraft.util.ChunkCoordinates;
 import net.minecraft.world.World;
 
 public class DungeonManager
 {
-    public static boolean starter;
-    
     public static boolean generate(World world, Random random, int x, int y, int z)
     {
-        if(starter)
+        ChunkCoordinates ck = world.getSpawnPoint();
+        
+        if(ck.posX == 0 && ck.posY == 128 && ck.posZ == 0)
         {
-            return (new StarterDungeon()).generate(world, random, x, y, z);
+            return new StarterDungeon().generate(world, random, x, y, z);
         }
         else
         {
-            switch(random.nextInt(5))
-            {
-                case 0: return ((new VanillaDungeon()).generate(world, random, x, y, z));
-                case 1: return ((new VillageDungeon()).generate(world, random, x, y, z));
-                case 2: return ((new DesertDungeon()).generate(world, random, x, y, z));
-                case 3: return ((new JungleDungeon()).generate(world, random, x, y, z));
-                case 4: return ((new NetherDungeon()).generate(world, random, x, y, z));
-                default: return false;
-            }
+            return new VanillaDungeon().generate(world, random, x, y, z);
         }
     }
 }
