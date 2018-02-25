2d9b265df6146c28498894375795d3802fdf4a09
StormTiberius <equestrian@equestria.net>
2018-02-25 21:36:59 +0200

    Biome Final

--- cde/ember/BiomeGenEmber.java
+++ cde/ember/BiomeGenEmber.java
@@ -23,7 +23,7 @@ public class BiomeGenEmber extends BiomeGenBase
     public void decorate(World par1World, Random par2Random, int par3, int par4)
     {
         super.decorate(par1World, par2Random, par3, par4);
-        int var5 = 3 + par2Random.nextInt(6);
+        int var5 = (3 + par2Random.nextInt(6)) * 2;
         int var6;
         int var7;
         int var8;
@@ -31,7 +31,7 @@ public class BiomeGenEmber extends BiomeGenBase
         for (var6 = 0; var6 < var5; ++var6)
         {
             var7 = par3 + par2Random.nextInt(16);
-            var8 = par2Random.nextInt(28) + 4;
+            var8 = par2Random.nextInt(56) + 4;
             int var9 = par4 + par2Random.nextInt(16);
             int var10 = par1World.getBlockId(var7, var8, var9);
 
