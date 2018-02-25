805bc546fbfd7d82c6711505e9b132faf1c8e075
StormTiberius <equestrian@equestria.net>
2018-02-25 16:32:02 +0200

    MsSh Y Mod

--- cde/ember/StructureMineshaftStart.java
+++ cde/ember/StructureMineshaftStart.java
@@ -1,8 +1,10 @@
 package cde.ember;
 
+import java.util.Iterator;
 import java.util.Random;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.structure.ComponentMineshaftRoom;
+import net.minecraft.world.gen.structure.StructureComponent;
 import net.minecraft.world.gen.structure.StructureStart;
 
 public class StructureMineshaftStart extends StructureStart
@@ -14,5 +16,27 @@ public class StructureMineshaftStart extends StructureStart
         var5.buildComponent(var5, this.components, par2Random);
         this.updateBoundingBox();
         this.markAvailableHeight(par1World, par2Random, 10);
+    }
+    
+    @Override
+    protected void markAvailableHeight(World par1World, Random par2Random, int par3)
+    {
+        int var4 = 255 - par3;
+        int var5 = this.boundingBox.getYSize() + 1;
+
+        if (var5 < var4)
+        {
+            var5 += par2Random.nextInt(var4 - var5);
+        }
+
+        int var6 = var5 - this.boundingBox.maxY;
+        this.boundingBox.offset(0, var6, 0);
+        Iterator var7 = this.components.iterator();
+
+        while (var7.hasNext())
+        {
+            StructureComponent var8 = (StructureComponent)var7.next();
+            var8.getBoundingBox().offset(0, var6, 0);
+        }
     }
 }
