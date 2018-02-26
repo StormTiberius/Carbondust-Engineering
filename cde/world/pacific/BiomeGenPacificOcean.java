54e092c960d6ee79ffcbfd0bc1a31a7b0b2c64ad
StormTiberius <equestrian@equestria.net>
2018-02-27 00:55:01 +0200

    Revert "Removed World"
    This reverts commit 0e6b8ade86eae66b940ba8dbea59664ddf213efd.

---
+++ cde/world/pacific/BiomeGenPacificOcean.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author StormTiberius
+ */
+
+package cde.world.pacific;
+
+public class BiomeGenPacificOcean extends BiomeGenPacific
+{
+    public BiomeGenPacificOcean(int id)
+    {
+        super(id);
+        
+        theBiomeDecorator.treesPerChunk = -999;
+    }
+}
