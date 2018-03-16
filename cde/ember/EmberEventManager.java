eb7ebcfc0849d61b5fe372d9c936afd3bc0a3824
StormTiberius <equestrian@equestria.net>
2018-03-16 18:05:19 +0200

    Dimension p2

--- cde/ember/EmberEventManager.java
+++ cde/ember/EmberEventManager.java
@@ -11,10 +11,17 @@ import net.minecraftforge.event.entity.player.PlayerSleepInBedEvent;
 
 public class EmberEventManager
 {
+    private final int dimensionId;
+    
+    public EmberEventManager(int dimensionId)
+    {
+        this.dimensionId = dimensionId;
+    }
+    
     @ForgeSubscribe
     public void psibe(PlayerSleepInBedEvent event)
     {
-        if(!event.entityPlayer.worldObj.isRemote)
+        if(!event.entityPlayer.worldObj.isRemote && event.entityPlayer.dimension == dimensionId)
         {
             event.entityPlayer.setSpawnChunk(new ChunkCoordinates(event.x, event.y, event.z), false);
         }
