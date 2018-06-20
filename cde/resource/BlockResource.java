ac3beedd6758551e54a4354429f9dd857fad4c3f
StormTiberius <me@co.fi>
2018-06-20 15:51:13 +0300

    Resource Blocks

--- cde/resource/BlockResource.java
+++ cde/resource/BlockResource.java
@@ -5,10 +5,10 @@
 
 package cde.resource;
 
+import cde.ResourceCore;
 import java.util.Random;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.world.World;
 
 public class BlockResource extends Block
@@ -16,26 +16,28 @@ public class BlockResource extends Block
     public BlockResource(int id)
     {
         super(id, 17, Material.rock);
-        setBlockUnbreakable();
-        setResistance(6000000.0F);
-        setStepSound(soundStoneFootstep);
-        disableStats();
-        setTickRandomly(true);
-        setCreativeTab(CreativeTabs.tabBlock);
+        enableStats = false;
     }
-
+        
     @Override
     public void updateTick(World world, int x, int y, int z, Random random)
     {
-        setResource(world, x, y, z);
-        world.scheduleBlockUpdate(x, y, z, this.blockID, 5);
+        if(!world.isRemote)
+        {
+            setResource(world, x, y, z);
+            world.scheduleBlockUpdate(x, y, z, blockID, ResourceCore.resourceRate);
+        }
     }
     
     @Override
     public void onBlockAdded(World world, int x, int y, int z)
     {
         super.onBlockAdded(world, x, y, z);
-        world.scheduleBlockUpdate(x, y, z, this.blockID, 5);
+        
+        if(!world.isRemote)
+        {
+            world.scheduleBlockUpdate(x, y, z, blockID, ResourceCore.resourceRate);
+        }
     }
 
     private void setResource(World world, int x, int y, int z)
@@ -43,7 +45,7 @@ public class BlockResource extends Block
         if(world.isAirBlock(x, y + 1, z))
         {
             WeightedObject wo = ResourceManager.getResource();
-            
+
             world.setBlockAndMetadataWithNotify(x, y + 1, z, wo.objectId, wo.objectMeta);
         }
     }
