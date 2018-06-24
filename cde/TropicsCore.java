9406384219c15871edfff2114582f1a88e1f319a
StormTiberius <me@co.fi>
2018-06-24 20:00:20 +0300

    Tropics S1

--- cde/TropicsCore.java
+++ cde/TropicsCore.java
@@ -10,7 +10,7 @@ import cde.tropics.BiomeGenTropicsBeach;
 import cde.tropics.BiomeGenTropicsIsland;
 import cde.tropics.BiomeGenTropicsOcean;
 import cde.tropics.WorldChunkManagerTropics;
-import cde.tropics.WorldTypeTropics;
+import cde.tropics.WorldProviderTropics;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.PostInit;
@@ -21,21 +21,23 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.network.NetworkMod;
-import cpw.mods.fml.common.registry.LanguageRegistry;
 import java.io.File;
-import net.minecraft.world.WorldType;
+import net.minecraft.block.Block;
 import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraftforge.common.Configuration;
+import net.minecraftforge.common.DimensionManager;
+import net.minecraftforge.liquids.LiquidContainerRegistry;
+import net.minecraftforge.liquids.LiquidDictionary;
+import net.minecraftforge.liquids.LiquidStack;
 
 @Mod(modid="CDE|Tropics", name="Tropics", version=Version.VERSION, dependencies = "required-after:Forge@[6.6.2.534,);required-after:CDE|Core")
 @NetworkMod(clientSideRequired=true, serverSideRequired=true)
 public class TropicsCore
 {
     private static Configuration cfg;
-    private static int islandSize,islandScarcity,islandId,beachId,oceanId,tropicsId;
-    
+    private static boolean enabled;
+    private static int islandSize,islandScarcity,islandId,beachId,oceanId,dimensionId,liquidId;
     public static BiomeGenBase island,beach,ocean;
-    public static WorldType tropics;
     
     @PreInit
     public void preInit(FMLPreInitializationEvent event) 
@@ -44,44 +46,78 @@ public class TropicsCore
         
         cfg.load();
         
+        enabled = cfg.get(Configuration.CATEGORY_GENERAL, "enabled", false, "Enable/Disable Tropics").getBoolean(false);
+        
+        islandId = cfg.get(Configuration.CATEGORY_GENERAL, "islandid", 23, "Island biome id").getInt();
+        beachId = cfg.get(Configuration.CATEGORY_GENERAL, "beachid", 24, "Beach biome id").getInt();
+        oceanId = cfg.get(Configuration.CATEGORY_GENERAL, "oceanid", 25, "Ocean biome id").getInt();
+        
+        dimensionId = cfg.get(Configuration.CATEGORY_GENERAL, "dimensionid", 2, "Tropics dimension id").getInt();
+        
         islandSize = cfg.get(Configuration.CATEGORY_GENERAL, "islandsize", 4, "Island size, 4-6 recommended").getInt();
         islandScarcity = cfg.get(Configuration.CATEGORY_GENERAL, "islandscarcity", 100, "Island scarcity, 100 default").getInt();
-        
-        islandId = cfg.get(Configuration.CATEGORY_GENERAL, "islandid", 23, "Island Biome ID").getInt();
-        beachId = cfg.get(Configuration.CATEGORY_GENERAL, "beachid", 24, "Beach Biome ID").getInt();
-        oceanId = cfg.get(Configuration.CATEGORY_GENERAL, "oceanid", 25, "Ocean Biome ID").getInt();
-        
-        tropicsId = cfg.get(Configuration.CATEGORY_GENERAL, "tropicsid", 10, "Tropics ID").getInt();
 
         cfg.save();
       
-        island = (new BiomeGenTropicsIsland(islandId)).setColor(16440917).setBiomeName("Tropics").setTemperatureRainfall(0.8F, 0.4F).setMinMaxHeight(0.0F, 0.1F);
-        beach = (new BiomeGenTropicsBeach(beachId)).setColor(16440917).setBiomeName("Tropics").setTemperatureRainfall(0.8F, 0.4F).setMinMaxHeight(0.0F, 0.1F);
-        ocean = (new BiomeGenTropicsOcean(oceanId)).setColor(16440917).setBiomeName("Tropics").setTemperatureRainfall(0.8F, 0.4F).setMinMaxHeight(-1.0F, 0.1F);
-        
-        WorldChunkManagerTropics.allowedBiomes.clear();
-        WorldChunkManagerTropics.allowedBiomes.add(island);
-        WorldChunkManagerTropics.allowedBiomes.add(beach);
-        
-        tropics = new WorldTypeTropics(tropicsId, "TROPICS");
+        if(enabled)
+        {
+            island = (new BiomeGenTropicsIsland(islandId)).setColor(16440917).setBiomeName("Tropics").setTemperatureRainfall(0.8F, 0.4F).setMinMaxHeight(0.0F, 0.1F);
+            beach = (new BiomeGenTropicsBeach(beachId)).setColor(16440917).setBiomeName("Tropics").setTemperatureRainfall(0.8F, 0.4F).setMinMaxHeight(0.0F, 0.1F);
+            ocean = (new BiomeGenTropicsOcean(oceanId)).setColor(16440917).setBiomeName("Tropics").setTemperatureRainfall(0.8F, 0.4F).setMinMaxHeight(-1.0F, 0.1F);
+            
+            WorldChunkManagerTropics.allowedBiomes.clear();
+            WorldChunkManagerTropics.allowedBiomes.add(island);
+            WorldChunkManagerTropics.allowedBiomes.add(beach);
+        }
     }
 
     @Init
     public void init(FMLInitializationEvent event) 
     {   
-        LanguageRegistry.instance().addStringLocalization("generator.TROPICS", "en_US", "Tropics");
+        if(enabled)
+        {
+            if(dimensionId == 0 || dimensionId == -1 || dimensionId == 1)
+            {
+                DimensionManager.unregisterProviderType(dimensionId);
+                DimensionManager.registerProviderType(dimensionId, WorldProviderTropics.class, true);
+            }
+            else
+            {
+                DimensionManager.registerProviderType(dimensionId, WorldProviderTropics.class, true);
+                DimensionManager.registerDimension(dimensionId, dimensionId);
+            }
+        }
     }
 
     @PostInit
     public void postInit(FMLPostInitializationEvent event) 
     {
+        LiquidStack oil = LiquidDictionary.getLiquid("Oil", LiquidContainerRegistry.BUCKET_VOLUME);
         
+        if(oil != null)
+        {
+            liquidId = oil.itemID;
+        }
+        else
+        {
+            liquidId = Block.lavaStill.blockID;
+        }
     }
 
     @ServerStarting
     public void serverStarting(FMLServerStartingEvent event)
     {
         
+    }
+    
+    public static int getLiquidId()
+    {
+        return liquidId;
+    }
+    
+    public static int getDimensionId()
+    {
+        return dimensionId;
     }
     
     public static byte getIslandSize()
