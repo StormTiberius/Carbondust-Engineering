6f8cd5f00056e12b0bf6e9720471a3f13f0d2be2
StormTiberius <me@co.fi>
2018-06-24 20:14:35 +0300

    Tropics S5

--- cde/tropics/BiomeGenTropics.java
+++ cde/tropics/BiomeGenTropics.java
@@ -7,6 +7,9 @@ package cde.tropics;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import java.util.Random;
+import net.minecraft.block.Block;
+import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
 
 public abstract class BiomeGenTropics extends BiomeGenBase
@@ -15,12 +18,30 @@ public abstract class BiomeGenTropics extends BiomeGenBase
     {
         super(id);
 
-        spawnableCreatureList.clear();
-        spawnableMonsterList.clear();
-        spawnableWaterCreatureList.clear();
-        field_82914_M.clear();
-        
-        theBiomeDecorator.generateLakes = false;
+        theBiomeDecorator = new BiomeDecoratorTropics(this);
+    }
+    
+    @Override
+    public void decorate(World par1World, Random par2Random, int par3, int par4)
+    {
+        super.decorate(par1World, par2Random, par3, par4);
+        int var5 = (3 + par2Random.nextInt(6)) * 2;
+        int var6;
+        int var7;
+        int var8;
+
+        for (var6 = 0; var6 < var5; ++var6)
+        {
+            var7 = par3 + par2Random.nextInt(16);
+            var8 = par2Random.nextInt(56) + 4;
+            int var9 = par4 + par2Random.nextInt(16);
+            int var10 = par1World.getBlockId(var7, var8, var9);
+
+            if (var10 == Block.stone.blockID)
+            {
+                par1World.setBlock(var7, var8, var9, Block.oreEmerald.blockID);
+            }
+        }
     }
     
     @SideOnly(Side.CLIENT)
