b699b78693cf71a863cb5367bc90b04c8fea24ff
StormTiberius <me@co.fi>
2018-06-25 20:18:26 +0300

    Default adjust

--- cde/core/Defaults.java
+++ cde/core/Defaults.java
@@ -137,7 +137,7 @@ public class Defaults
     
     public static final int[][] ORE_GEN_DEFAULTS =
     {    
-        new int[]{1,8,10,20,55},
+        new int[]{1,8,10,40,75},
         new int[]{1,8,7,20,55},
         new int[]{1,8,3,5,30},
         new int[]{1,8,4,10,35},        
@@ -145,7 +145,7 @@ public class Defaults
         new int[]{1,6,1,10,16},
         new int[]{1,6,1,16,32},
         new int[]{1,4,7,12,62},
-        new int[]{1,6,1,0,64},
+        new int[]{1,6,1,64,128},
         new int[]{1,7,2,0,48},
         new int[]{1,7,2,0,48},
         new int[]{1,7,2,0,48}
