dd0497766b39fbcdf5c5952aa8a38cebb04adf28
StormTiberius <me@co.fi>
2018-06-27 20:45:31 +0300

    Tropics B9

--- cde/core/Defaults.java
+++ cde/core/Defaults.java
@@ -137,15 +137,15 @@ public class Defaults
     
     public static final int[][] ORE_GEN_DEFAULTS =
     {    
-        new int[]{1,8,10,40,75},
-        new int[]{1,8,7,20,55},
-        new int[]{1,8,3,5,30},
+        new int[]{1,8,20,0,64},
+        new int[]{1,8,10,0,48},
+        new int[]{1,8,4,0,32},
         new int[]{1,8,4,10,35},        
         new int[]{1,3,20,0,64},
         new int[]{1,6,1,10,16},
         new int[]{1,6,1,16,32},
         new int[]{1,4,7,12,62},
-        new int[]{1,6,1,64,128},
+        new int[]{1,6,1,16,64},
         new int[]{1,7,2,0,48},
         new int[]{1,7,2,0,48},
         new int[]{1,7,2,0,48}
--- cde/core/Version.java
+++ cde/core/Version.java
@@ -7,5 +7,5 @@ package cde.core;
 
 public class Version
 {
-    public static final String VERSION = "1.0.11";
+    public static final String VERSION = "1.0.12";
 }
