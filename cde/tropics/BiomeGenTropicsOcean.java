230bf38096ba1d3558fab274b6db3a843478139e
StormTiberius <me@co.fi>
2018-06-27 16:25:23 +0300

    Tropics B4

--- cde/tropics/BiomeGenTropicsOcean.java
+++ cde/tropics/BiomeGenTropicsOcean.java
@@ -5,8 +5,6 @@
 
 package cde.tropics;
 
-import net.minecraft.world.biome.SpawnListEntry;
-
 public class BiomeGenTropicsOcean extends BiomeGenTropics
 {
     public BiomeGenTropicsOcean(int id)
@@ -14,8 +12,6 @@ public class BiomeGenTropicsOcean extends BiomeGenTropics
         super(id);
         
         spawnableCreatureList.clear();
-        
-        spawnableWaterCreatureList.add(new SpawnListEntry(EntitySquiddy.class, 10, 4, 4));
         
         theBiomeDecorator.treesPerChunk = -999;
     }
