1b0c572bf73e554295b6dfab10ba4cef052cc8ac
StormTiberius <me@co.fi>
2018-06-28 23:19:44 +0300

    Revert "Tropics B17"
    This reverts commit 604756f05cf180a8175a78c60d2f6a01266fc883.

--- cde/tropics/BiomeGenTropicsBeach.java
+++ cde/tropics/BiomeGenTropicsBeach.java
@@ -5,11 +5,6 @@
 
 package cde.tropics;
 
-import ic2.core.block.WorldGenRubTree;
-import java.util.Random;
-import net.minecraft.src.ModLoader;
-import net.minecraft.world.World;
-
 public class BiomeGenTropicsBeach extends BiomeGenTropics
 {
     public BiomeGenTropicsBeach(int id)
@@ -23,21 +18,5 @@ public class BiomeGenTropicsBeach extends BiomeGenTropics
         theBiomeDecorator.treesPerChunk = 50;
         theBiomeDecorator.reedsPerChunk = 50;
         theBiomeDecorator.cactiPerChunk = 10;
-    }
-    
-    @Override
-    public void decorate(World par1World, Random par2Random, int par3, int par4)
-    {
-        super.decorate(par1World, par2Random, par3, par4);
-        
-        if(ModLoader.isModLoaded("IC2"))
-        {
-            int rubbertrees = par2Random.nextInt(5) + 1;
-
-            if(par2Random.nextInt(100) + 1 <= rubbertrees * 2)
-            {
-                (new WorldGenRubTree()).generate(par1World, par2Random, par3 + par2Random.nextInt(16), rubbertrees, par4 + par2Random.nextInt(16));
-            }
-        }
     }
 }
