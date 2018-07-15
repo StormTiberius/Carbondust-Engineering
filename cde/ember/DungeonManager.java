d79574fe0ae40881c7a079b22790e060dc5e6421
StormTiberius <me@co.fi>
2018-07-15 10:29:08 +0300

    LocD

--- cde/ember/DungeonManager.java
+++ cde/ember/DungeonManager.java
@@ -7,17 +7,25 @@ package cde.ember;
 
 import java.util.Random;
 import net.minecraft.block.Block;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.ChunkCoordinates;
+import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ChestGenHooks;
 
 public class DungeonManager
 {   
+    private static final AxisAlignedBB BB = AxisAlignedBB.getBoundingBox(192, 0, 192, 336, 255, 336);
+    
     public static boolean generate(World world, Random random, int x, int y, int z)
     {   
         ChunkCoordinates cc = world.provider.getSpawnPoint();
         
         if(cc.posX == LocationData.SPAWN_X && cc.posY == LocationData.SPAWN_Y && cc.posZ == LocationData.SPAWN_Z)
+        {
+            return new WorldGenDungeons(ChestGenHooks.BONUS_CHEST, Block.grass.blockID, Block.cobblestone.blockID).generate(world, random, x, random.nextInt(128), z);
+        }
+        else if(BB.isVecInside(Vec3.createVectorHelper(x, y, z)) && !BB.isVecInside(Vec3.createVectorHelper(cc.posX, cc.posY, cc.posZ)))
         {
             return new WorldGenDungeons(ChestGenHooks.BONUS_CHEST, Block.grass.blockID, Block.cobblestone.blockID).generate(world, random, x, random.nextInt(128), z);
         }
