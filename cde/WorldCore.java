b09b9ecd1b4064ea5c620e7253730d063758c06f
StormTiberius <me@co.fi>
2018-07-17 12:48:09 +0300

    W2

--- cde/WorldCore.java
+++ cde/WorldCore.java
@@ -31,31 +31,38 @@ import net.minecraftforge.liquids.LiquidContainerRegistry;
 import net.minecraftforge.liquids.LiquidDictionary;
 import net.minecraftforge.liquids.LiquidStack;
 
-@Mod(modid="CDE|Ember", name="Ember", version=Version.VERSION, dependencies = "required-after:Forge@[6.6.2.534,);required-after:CDE|Core")
+@Mod(modid="CDE|World", name="World", version=Version.VERSION, dependencies = "required-after:Forge@[6.6.2.534,);required-after:CDE|Core")
 @NetworkMod(clientSideRequired=true, serverSideRequired=true)
 public class WorldCore
 {   
     private static Configuration cfg;
-    private static boolean enabled;
-    private static int biomeId,dimensionId,liquidId;
+    private static boolean tropics,cave,laputa;
+    private static int islandBiomeId,beachBiomeId,oceanBiomeId,caveBiomeId,laputaBiomeId,tropicsDimensionId,caveDimensionId,laputaDimensionId,liquidId;
     public static BiomeGenBase ember;
     public static final String EMBER_SPAWN_LOCATION_KEYWORD = "EmberSpawnLocation";
     
     @PreInit
     public void preInit(FMLPreInitializationEvent event) 
     {
-        cfg = new Configuration(new File(event.getModConfigurationDirectory(), "cde/ember.cfg"));
+        cfg = new Configuration(new File(event.getModConfigurationDirectory(), "cde/world.cfg"));
         cfg.load();
         
-        enabled = cfg.get(Configuration.CATEGORY_GENERAL, "enabled", false, "Enable/Disable Ember").getBoolean(false);
-        biomeId = cfg.get(Configuration.CATEGORY_GENERAL, "biomeid", 23, "Ember biome id").getInt();
-        dimensionId = cfg.get(Configuration.CATEGORY_GENERAL, "dimensionid", 2, "Ember dimension id").getInt();
+        tropics = cfg.get(Configuration.CATEGORY_GENERAL, "tropics", false, "Enable/Disable Tropics Dimension").getBoolean(false);
+        cave = cfg.get(Configuration.CATEGORY_GENERAL, "cave", false, "Enable/Disable Cave Dimension").getBoolean(false);
+        laputa = cfg.get(Configuration.CATEGORY_GENERAL, "laputa", false, "Enable/Disable Laputa Dimension").getBoolean(false);
+        
+        tropicsDimensionId = cfg.get(Configuration.CATEGORY_GENERAL, "tropicsDimensionId", 2, "Tropics Dimension Id").getInt();
+        caveDimensionId = cfg.get(Configuration.CATEGORY_GENERAL, "caveDimensionId", 3, "Cave Dimension Id").getInt();
+        laputaDimensionId = cfg.get(Configuration.CATEGORY_GENERAL, "laputaDimensionId", 4, "Laputa Dimension Id").getInt();
+        
+        caveBiomeId = cfg.get(Configuration.CATEGORY_GENERAL, "caveBiomeId", 26, "Cave Biome Id").getInt();
+        laputaBiomeId = cfg.get(Configuration.CATEGORY_GENERAL, "laputaBiomeId", 27, "Laputa Biome Id").getInt();
         
         cfg.save();
         
-        if(enabled)
+        if(cave)
         {
-            ember = (new BiomeGenEmber(biomeId)).setColor(112).setBiomeName("Ember");
+            ember = (new BiomeGenEmber(caveBiomeId)).setColor(112).setBiomeName("Ember");
         }
       
     }
@@ -63,22 +70,22 @@ public class WorldCore
     @Init
     public void init(FMLInitializationEvent event) 
     {
-        if(enabled)
+        if(tropics)
         {
-            if(dimensionId == 0 || dimensionId == -1 || dimensionId == 1)
+            if(tropicsDimensionId == 0 || tropicsDimensionId == -1 || tropicsDimensionId == 1)
             {
-                DimensionManager.unregisterProviderType(dimensionId);
-                DimensionManager.registerProviderType(dimensionId, WorldProviderEmber.class, true);
+                DimensionManager.unregisterProviderType(tropicsDimensionId);
+                DimensionManager.registerProviderType(tropicsDimensionId, WorldProviderEmber.class, true);
             }
             else
             {
-                DimensionManager.registerProviderType(dimensionId, WorldProviderEmber.class, true);
-                DimensionManager.registerDimension(dimensionId, dimensionId);
+                DimensionManager.registerProviderType(tropicsDimensionId, WorldProviderEmber.class, true);
+                DimensionManager.registerDimension(tropicsDimensionId, tropicsDimensionId);
             }
             
-            GameRegistry.registerWorldGenerator(new WorldGenOil(dimensionId));
+            GameRegistry.registerWorldGenerator(new WorldGenOil(tropicsDimensionId));
             
-            MinecraftForge.EVENT_BUS.register(new EmberEventManager(dimensionId));
+            MinecraftForge.EVENT_BUS.register(new EmberEventManager(tropicsDimensionId));
         }
     }
 
@@ -110,6 +117,6 @@ public class WorldCore
     
     public static int getDimensionId()
     {
-        return dimensionId;
+        return caveDimensionId;
     }
 }
