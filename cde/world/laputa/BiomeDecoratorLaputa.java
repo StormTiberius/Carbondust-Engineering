81b43f1ac189fb2adccb2d84d3682c841d7671e5
StormTiberius <me@co.fi>
2018-07-17 15:32:54 +0300

    Laputa 2

---
+++ cde/world/laputa/BiomeDecoratorLaputa.java
@@ -0,0 +1,23 @@
+/**
+ *
+ * @author StormTiberius
+ */
+
+package cde.world.laputa;
+
+import net.minecraft.world.biome.BiomeDecorator;
+import net.minecraft.world.biome.BiomeGenBase;
+
+public class BiomeDecoratorLaputa extends BiomeDecorator
+{
+    public BiomeDecoratorLaputa(BiomeGenBase biome)
+    {
+        super(biome);
+    }
+    
+    @Override
+    protected void decorate()
+    {
+        
+    }
+}
