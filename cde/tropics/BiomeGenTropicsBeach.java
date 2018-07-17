de876f3372de49e34eb7e4bcc24bfcdae32fadeb
StormTiberius <me@co.fi>
2018-07-17 12:50:19 +0300

    Revert "Remove Tropics"
    This reverts commit e1ce76891280f3ac4da76a2acdbd700e15936dd2.

---
+++ cde/tropics/BiomeGenTropicsBeach.java
@@ -0,0 +1,22 @@
+/**
+ *
+ * @author StormTiberius
+ */
+
+package cde.tropics;
+
+public class BiomeGenTropicsBeach extends BiomeGenTropics
+{
+    public BiomeGenTropicsBeach(int id)
+    {
+        super(id);
+        
+        spawnableCreatureList.clear();
+        spawnableMonsterList.clear();
+        spawnableWaterCreatureList.clear();
+        
+        theBiomeDecorator.treesPerChunk = 50;
+        theBiomeDecorator.reedsPerChunk = 50;
+        theBiomeDecorator.cactiPerChunk = 10;
+    }
+}
