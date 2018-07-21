ad430e1a72c873c0a0947e546ced40e4583a66a9
StormTiberius <me@co.fi>
2018-07-21 11:50:58 +0300

    Fin

--- cde/laputa/WorldProviderLaputa.java
+++ cde/laputa/WorldProviderLaputa.java
@@ -140,7 +140,8 @@ public class WorldProviderLaputa extends WorldProvider
         {
             super.updateWeather();
         }
-        
+        else
+        {
             WorldInfo worldInfo = worldObj.getWorldInfo();
 
             time = worldInfo.getWorldTime();
@@ -237,6 +238,7 @@ public class WorldProviderLaputa extends WorldProvider
                     worldObj.thunderingStrength = 1.0F;
                 }
             }
+        }
     }
     
     @Override
