67bfbc9a1f9d0d6c713c8d3e3224438c4ea5e521
StormTiberius <me@co.fi>
2018-07-23 18:31:14 +0300

    TC2

--- cde/TropicsCore.java
+++ cde/TropicsCore.java
@@ -10,7 +10,7 @@ import cde.tropics.BiomeGenTropicsBeach;
 import cde.tropics.BiomeGenTropicsIsland;
 import cde.tropics.BiomeGenTropicsOcean;
 import cde.tropics.WorldChunkManagerTropics;
-import cde.tropics.WorldTypeTropics;
+import cde.tropics.WorldProviderTropics;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.PostInit;
@@ -34,7 +34,7 @@ public class TropicsCore
 {
     private static Configuration cfg;
     private static boolean enabled;
-    private static int islandId,beachId,oceanId,tropicsId,islandSize,islandScarcity;
+    private static int islandId,beachId,oceanId,dimensionId,islandSize,islandScarcity;
     public static BiomeGenBase island,beach,ocean;
     public static WorldType tropics;
     
@@ -50,32 +50,39 @@ public class TropicsCore
         
         cfg.load();
         
+        enabled = cfg.get(Configuration.CATEGORY_GENERAL, "enabled", false, "Enable/Disable Tropics Dimension").getBoolean(false);
+        
         islandId = cfg.get(Configuration.CATEGORY_GENERAL, "islandid", 23, "Island id").getInt();
         beachId = cfg.get(Configuration.CATEGORY_GENERAL, "beachid", 24, "Beach id").getInt();
         oceanId = cfg.get(Configuration.CATEGORY_GENERAL, "oceanid", 25, "Ocean id").getInt();
         
-        tropicsId = cfg.get(Configuration.CATEGORY_GENERAL, "tropicsid", 10, "Tropics id").getInt();
+        dimensionId = cfg.get(Configuration.CATEGORY_GENERAL, "dimensionId", 2, "Laputa Dimension Id").getInt();
+        
+        weatherDurations = cfg.get(Configuration.CATEGORY_GENERAL, "weatherDurations", WEATHER_DURATIONS, "Weather Durations").getIntList();
+        
+        dayCycleDurationMultiplier = cfg.get(Configuration.CATEGORY_GENERAL, "dayCycleDurationMultiplier", 1, "Day Cycle Multiplier").getInt();
         
         islandSize = cfg.get(Configuration.CATEGORY_GENERAL, "islandsize", 4, "Island size, 4-6 recommended").getInt();
         islandScarcity = cfg.get(Configuration.CATEGORY_GENERAL, "islandscarcity", 100, "Island scarcity, 100 default").getInt();
 
         cfg.save();
 
-        island = (new BiomeGenTropicsIsland(islandId)).setColor(16440917).setBiomeName("Tropics").setTemperatureRainfall(0.8F, 0.4F).setMinMaxHeight(0.0F, 0.1F);
-        beach = (new BiomeGenTropicsBeach(beachId)).setColor(16440917).setBiomeName("Tropics").setTemperatureRainfall(0.8F, 0.4F).setMinMaxHeight(0.0F, 0.1F);
-        ocean = (new BiomeGenTropicsOcean(oceanId)).setColor(16440917).setBiomeName("Tropics").setTemperatureRainfall(0.8F, 0.4F).setMinMaxHeight(-1.0F, 0.1F);
+        if(enabled)
+        {
+            island = (new BiomeGenTropicsIsland(islandId)).setColor(16440917).setBiomeName("Tropics").setTemperatureRainfall(0.8F, 0.4F).setMinMaxHeight(0.0F, 0.1F);
+            beach = (new BiomeGenTropicsBeach(beachId)).setColor(16440917).setBiomeName("Tropics").setTemperatureRainfall(0.8F, 0.4F).setMinMaxHeight(0.0F, 0.1F);
+            ocean = (new BiomeGenTropicsOcean(oceanId)).setColor(16440917).setBiomeName("Tropics").setTemperatureRainfall(0.8F, 0.4F).setMinMaxHeight(-1.0F, 0.1F);
 
-        WorldChunkManagerTropics.allowedBiomes.clear();
-        WorldChunkManagerTropics.allowedBiomes.add(island);
-        WorldChunkManagerTropics.allowedBiomes.add(beach);
-        
-        tropics = new WorldTypeTropics(tropicsId, "TROPICS");
+            WorldChunkManagerTropics.allowedBiomes.clear();
+            WorldChunkManagerTropics.allowedBiomes.add(island);
+            WorldChunkManagerTropics.allowedBiomes.add(beach);
+        }
     }
 
     @Init
     public void init(FMLInitializationEvent event) 
-    {        
-        LanguageRegistry.instance().addStringLocalization("generator.TROPICS", "en_US", "Tropics");
+    {   
+        
     }
 
     @PostInit
@@ -88,6 +95,26 @@ public class TropicsCore
     public void serverStarting(FMLServerStartingEvent event)
     {
         
+    }
+        
+    public static int getDimensionId()
+    {
+        return dimensionId;
+    }
+    
+    public static int getWeatherDuration(int index)
+    {
+        switch(index)
+        {
+            case 3:return weatherDurations[index] + weatherDurations[9];
+            case 7: return weatherDurations[index] + weatherDurations[8];
+            default: return weatherDurations[index];
+        }
+    }
+    
+    public static int getDayCycleDurationMultiplier()
+    {
+        return dayCycleDurationMultiplier;
     }
     
     public static byte getIslandSize()
