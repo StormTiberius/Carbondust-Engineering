c8b51e742cd08d589c4901e57e99e01cb30a0c52
StormTiberius <me@co.fi>
2019-01-20 19:53:29 +0200

    Update BiomeGenEmber.java

--- cde/ember/BiomeGenEmber.java
+++ cde/ember/BiomeGenEmber.java
@@ -5,6 +5,9 @@
 
 package cde.ember;
 
+import java.util.Random;
+import net.minecraft.block.Block;
+import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
 
 public class BiomeGenEmber extends BiomeGenBase
@@ -12,5 +15,33 @@ public class BiomeGenEmber extends BiomeGenBase
     public BiomeGenEmber(int id)
     {
         super(id);
+        
+        theBiomeDecorator = new BiomeDecoratorEmber(this);
+        
+        spawnableCreatureList.clear();
+    }
+    
+    @Override
+    public void decorate(World par1World, Random par2Random, int par3, int par4)
+    {
+        super.decorate(par1World, par2Random, par3, par4);
+        
+        int var5 = 6 + par2Random.nextInt(11);
+        int var6;
+        int var7;
+        int var8;
+
+        for(var6 = 0; var6 < var5; ++var6)
+        {
+            var7 = par3 + par2Random.nextInt(16);
+            var8 = par2Random.nextInt(56) + 8;
+            int var9 = par4 + par2Random.nextInt(16);
+            int var10 = par1World.getBlockId(var7, var8, var9);
+
+            if (var10 == Block.stone.blockID)
+            {
+                par1World.setBlock(var7, var8, var9, Block.oreEmerald.blockID);
+            }
+        }
     }
 }
