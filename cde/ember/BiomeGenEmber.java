ef1ae8b4960d944738fb480d6f1158cba5e91d2a
StormTiberius <me@co.fi>
2019-01-21 19:16:29 +0200

    Update BiomeGenEmber.java

--- cde/ember/BiomeGenEmber.java
+++ cde/ember/BiomeGenEmber.java
@@ -5,10 +5,12 @@
 
 package cde.ember;
 
+import cde.EmberCore;
 import java.util.Random;
 import net.minecraft.block.Block;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.biome.SpawnListEntry;
 
 public class BiomeGenEmber extends BiomeGenBase
 {
@@ -19,6 +21,11 @@ public class BiomeGenEmber extends BiomeGenBase
         theBiomeDecorator = new BiomeDecoratorEmber(this);
         
         spawnableCreatureList.clear();
+        spawnableWaterCreatureList.clear();
+        field_82914_M.clear();
+        
+        spawnableWaterCreatureList.add(new SpawnListEntry(EntitySquidEmber.class, 10, 4, 4));
+        field_82914_M.add(new SpawnListEntry(EntityBatEmber.class, 10, 8, 8));
     }
     
     @Override
@@ -41,6 +48,48 @@ public class BiomeGenEmber extends BiomeGenBase
             if(var10 == Block.stone.blockID)
             {
                 par1World.setBlock(var7, var8, var9, Block.oreEmerald.blockID);
+            }
+        }
+        
+        liquidPopulate(par1World, par2Random, par3, par4);
+    }
+    
+    public void liquidPopulate(World world, Random random, int xPos, int zPos)
+    {
+        boolean mediumDeposit = random.nextDouble() <= (0.15 / 100.0);
+        boolean largeDeposit = random.nextDouble() <= (0.005 / 100.0);
+
+        if(mediumDeposit || largeDeposit)
+        {
+            int yPos = 40 + random.nextInt(20);
+
+            int r = 0;
+
+            if(mediumDeposit)
+            {
+                r = 4 + random.nextInt(4);
+            }
+            else if(largeDeposit)
+            {
+                r = 8 + random.nextInt(9);
+            }
+
+            int b = r * r;
+
+            for(int x = -r; x <= r; x++)
+            {
+                for(int y = -r; y <= r; y++)
+                {
+                    for(int z = -r; z <= r; z++)
+                    {
+                        int a = x * x + y * y + z * z;
+
+                        if(a <= b)
+                        {
+                            world.setBlockWithNotify(xPos + x, yPos + y, zPos + z, EmberCore.getLiquidId());
+                        }
+                    }
+                }
             }
         }
     }
