b77a678f830f6c31018b7fb2443eea90c61b6758
StormTiberius <me@co.fi>
2019-01-21 18:56:37 +0200

    Create EntityBatEmber.java

---
+++ cde/ember/EntityBatEmber.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author StormTiberius
+ */
+
+package cde.ember;
+
+import net.minecraft.entity.passive.EntityBat;
+import net.minecraft.world.World;
+
+public class EntityBatEmber extends EntityBat
+{
+    public EntityBatEmber(World world)
+    {
+        super(world);
+    }
+}
