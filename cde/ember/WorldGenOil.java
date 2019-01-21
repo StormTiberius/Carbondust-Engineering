2855e137796cca60bdf2e1ef2f5dbe5ecc743436
StormTiberius <me@co.fi>
2019-01-21 10:44:12 +0200

    Create WorldGenOil.java

---
+++ cde/ember/WorldGenOil.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * @author StormTiberius
+ */
+
+package cde.ember;
+
+import cpw.mods.fml.common.IWorldGenerator;
+import java.util.Random;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+
+public class WorldGenOil implements IWorldGenerator
+{
+    @Override
+    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider)
+    {
+        
+    }
+}
