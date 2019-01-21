ecb794f49f44c42dad7757f7d4b2cc9d9fc38443
StormTiberius <me@co.fi>
2019-01-21 18:57:11 +0200

    Create EntitySquidEmber.java

---
+++ cde/ember/EntitySquidEmber.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author StormTiberius
+ */
+
+package cde.ember;
+
+import net.minecraft.entity.passive.EntitySquid;
+import net.minecraft.world.World;
+
+public class EntitySquidEmber extends EntitySquid
+{
+    public EntitySquidEmber(World world)
+    {
+        super(world);
+    }
+}
