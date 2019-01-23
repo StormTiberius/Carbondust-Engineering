6fd3a0cd9e8350f0f2c2d620231fdc8a1af9fd38
StormTiberius <me@co.fi>
2019-01-23 16:23:31 +0200

    Update SoundEventManager.java

--- cde/core/sound/SoundEventManager.java
+++ cde/core/sound/SoundEventManager.java
@@ -18,17 +18,6 @@ public class SoundEventManager
     {
         SoundHelper.initSoundHelper();
         
-        if(CDECore.altRainSounds())
-        {
-            for(int i = 1; i < 9; i++)
-            {
-                String name = "cde/ambient/weather/rain" + i + ".ogg";
-                String path = "/cde/sounds/ambient/weather/rain" + i + ".ogg";
-
-                event.manager.soundPoolSounds.addSound(name, CDECore.class.getResource((new StringBuilder()).append(path).toString()));
-            }
-        }
-        
         if(CDECore.altExplosionSounds())
         {
             for(int i = 1; i < 5; i++)
@@ -49,15 +38,7 @@ public class SoundEventManager
     
     @ForgeSubscribe
     public void sem(PlaySoundEvent event)
-    {
-        if(CDECore.altRainSounds())
-        {
-            if(event.name.equalsIgnoreCase("ambient.weather.rain"))
-            {
-                event.result = event.manager.soundPoolSounds.getRandomSoundFromSoundPool("cde.ambient.weather.rain");
-            }
-        }
-        
+    {        
         if(CDECore.altExplosionSounds())
         {
             if(event.name.equalsIgnoreCase("random.explode"))
