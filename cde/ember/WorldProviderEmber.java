399567ba7f30fefd6af421fdf7fce584ae69f5ac
StormTiberius <me@co.fi>
2019-01-30 14:47:25 +0200

    Update WorldProviderEmber.java

--- cde/ember/WorldProviderEmber.java
+++ cde/ember/WorldProviderEmber.java
@@ -11,11 +11,10 @@ import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.util.ChunkCoordinates;
 import net.minecraft.world.WorldProvider;
 import net.minecraft.world.chunk.IChunkProvider;
-import net.minecraft.world.storage.WorldInfo;
 
 public class WorldProviderEmber extends WorldProvider
 {
-    private LocationData ld = new LocationData(EmberCore.EMBER_SPAWN_LOCATION_KEYWORD);
+    private static final ChunkCoordinates SPAWN = new ChunkCoordinates(264,20,264);
     
     @Override
     public String getDimensionName()
@@ -28,13 +27,6 @@ public class WorldProviderEmber extends WorldProvider
     {
         worldChunkMgr = new WorldChunkManagerEmber(EmberCore.ember, 0.8F, 0.4F);
         hasNoSky = true;
-        
-        LocationData data = (LocationData)worldObj.loadItemData(LocationData.class, EmberCore.EMBER_SPAWN_LOCATION_KEYWORD);
-        
-        if(data != null)
-        {
-            ld = data;
-        }
     }
     
     @Override
@@ -90,22 +82,13 @@ public class WorldProviderEmber extends WorldProvider
     @Override
     public ChunkCoordinates getSpawnPoint()
     {
-        ChunkCoordinates cc = ld.getSpawnLocation();
-        WorldInfo info = worldObj.getWorldInfo();
-        
-        if(cc.posX != info.getSpawnX() || cc.posY != info.getSpawnY() || cc.posZ != info.getSpawnZ())
-        {
-            info.setSpawnPosition(cc.posX, cc.posY, cc.posZ);
-        }
-        
-        return cc;
+        return SPAWN;
     }
     
     @Override
     public void setSpawnPoint(int x, int y, int z)
     {
-        ChunkCoordinates cc = ld.getSpawnLocation();
-        worldObj.getWorldInfo().setSpawnPosition(cc.posX, cc.posY, cc.posZ);
+        worldObj.getWorldInfo().setSpawnPosition(SPAWN.posX, SPAWN.posY, SPAWN.posZ);
     }
     
     @Override
@@ -124,10 +107,5 @@ public class WorldProviderEmber extends WorldProvider
     public double getHorizon()
     {
         return 127.0D;
-    }
-    
-    public void setLocationData(LocationData ld)
-    {
-        this.ld = ld;
     }
 }
