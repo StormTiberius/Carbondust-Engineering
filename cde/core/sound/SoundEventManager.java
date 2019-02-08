02eb2f2763c534d06b0024d93cb1b33faa44c667
StormTiberius <me@co.fi>
2019-02-08 16:47:26 +0200

    Revert "Update SoundEventManager.java"
    This reverts commit 6fd3a0cd9e8350f0f2c2d620231fdc8a1af9fd38.

--- cde/core/sound/SoundEventManager.java
+++ cde/core/sound/SoundEventManager.java
@@ -18,6 +18,17 @@ public class SoundEventManager
     {
         SoundHelper.initSoundHelper();
         
+        if(CDECore.altRainSounds())
+        {
+            for(int i = 1; i < 9; i++)
+            {
+                String name = "cde/ambient/weather/rain" + i + ".ogg";
+                String path = "/cde/sounds/ambient/weather/rain" + i + ".ogg";
+
+                event.manager.soundPoolSounds.addSound(name, CDECore.class.getResource((new StringBuilder()).append(path).toString()));
+            }
+        }
+        
         if(CDECore.altExplosionSounds())
         {
             for(int i = 1; i < 5; i++)
@@ -38,7 +49,15 @@ public class SoundEventManager
     
     @ForgeSubscribe
     public void sem(PlaySoundEvent event)
-    {        
+    {
+        if(CDECore.altRainSounds())
+        {
+            if(event.name.equalsIgnoreCase("ambient.weather.rain"))
+            {
+                event.result = event.manager.soundPoolSounds.getRandomSoundFromSoundPool("cde.ambient.weather.rain");
+            }
+        }
+        
         if(CDECore.altExplosionSounds())
         {
             if(event.name.equalsIgnoreCase("random.explode"))
