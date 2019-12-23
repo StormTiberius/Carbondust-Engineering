92d9bb71ba3b86001b2f9fa4bb82821a51be25f6
StormTiberius <storm@tiberius>
2019-12-23 17:12:54 +0200

    Proto 1

--- cde/industry/ItemBlockDrum.java
+++ cde/industry/ItemBlockDrum.java
@@ -6,60 +6,83 @@
 package cde.industry;
 
 import java.util.List;
-import java.util.Map;
-import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.liquids.LiquidContainerRegistry;
-import net.minecraftforge.liquids.LiquidDictionary;
+import net.minecraftforge.liquids.ILiquidTank;
 import net.minecraftforge.liquids.LiquidStack;
 
-public class ItemBlockDrum extends ItemBlock
+public class ItemBlockDrum extends ItemBlock implements ILiquidTank
 {
     public ItemBlockDrum(int id)
     {
         super(id);
     }
     
-    public LiquidStack getLiquid(ItemStack container)
+    @Override
+    public void addInformation(ItemStack is, EntityPlayer player, List list, boolean flag)
     {
-        return LiquidContainerRegistry.getLiquidForFilledItem(container);
+        
     }
     
     @Override
-    public int getMaxDamage()
+    public String getItemDisplayName(ItemStack is)
+    {
+        return "";
+    }
+    
+    /**
+     * @return LiquidStack representing the liquid contained in the tank, null if empty.
+     */
+    @Override
+    public LiquidStack getLiquid()
+    {
+        return null;
+    }
+
+    /**
+     * @return capacity of this tank
+     */
+    @Override
+    public int getCapacity()
+    {
+        return 0;
+    }
+
+    /**
+     *
+     * @param resource
+     * @param doFill
+     * @return Amount of liquid used for filling.
+     */
+    @Override
+    public int fill(LiquidStack resource, boolean doFill)
     {
         return 0;
     }
     
+    /**
+     *
+     * @param maxDrain
+     * @param doDrain
+     * @return Null if nothing was drained, otherwise a LiquidStack containing the drained.
+     */
     @Override
-    public boolean getHasSubtypes()
+    public LiquidStack drain(int maxDrain, boolean doDrain)
     {
-        return true;
-    }
-    
-    @Override
-    public int getMetadata(int meta)
-    {
-        return meta;
-    }
-
-    @Override
-    public String getItemNameIS(ItemStack is)
-    {
-        
-            return "NONAME";
-        
-        
-        
+        return null;
     }
 
+    /**
+     * Positive values indicate a positive liquid pressure (liquid wants to leave this tank)
+     * Negative values indicate a negative liquid pressure (liquid wants to fill this tank)
+     * Zero indicates no pressure
+     *
+     * @return a number indicating tank pressure
+     */
     @Override
-    public void getSubItems(int par1, CreativeTabs par2CreativeTabs, List par3List)
+    public int getTankPressure()
     {
-         
-        
-            par3List.add(new ItemStack(par1, 1, 0));
-        
+        return 0;
     }
 }
