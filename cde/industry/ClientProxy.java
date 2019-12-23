16b9f59ac16019021760317c0f9b1a28c4749289
StormTiberius <storm@tiberius>
2019-12-23 23:44:51 +0200

    Update ClientProxy.java

--- cde/industry/ClientProxy.java
+++ cde/industry/ClientProxy.java
@@ -6,15 +6,22 @@
 package cde.industry;
 
 import cde.IndustryCore;
-import cde.industry.render.RenderDrum;
+import cde.industry.render.RenderBlockDrum;
+import cde.industry.render.RenderItemBlockDrum;
 import cpw.mods.fml.client.registry.RenderingRegistry;
+import net.minecraftforge.client.MinecraftForgeClient;
 
 public class ClientProxy extends CommonProxy
 {   
     @Override
     public void registerRenderers()
     {
-        IndustryCore.setDrumRenderId(RenderingRegistry.getNextAvailableRenderId());
-        RenderingRegistry.registerBlockHandler(IndustryCore.getDrumRenderId(), new RenderDrum());
+        if(IndustryCore.isDrumEnabled())
+        {
+            IndustryCore.setDrumRenderId(RenderingRegistry.getNextAvailableRenderId());
+            RenderingRegistry.registerBlockHandler(IndustryCore.getDrumRenderId(), new RenderBlockDrum());
+
+            MinecraftForgeClient.registerItemRenderer(IndustryCore.blockDrum.blockID, new RenderItemBlockDrum());
+        }
     }
 }
