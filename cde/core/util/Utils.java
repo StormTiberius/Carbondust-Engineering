192bfa09468349548b95ecfd73be5d95bd1f8703
StormTiberius <storm@tiberius>
2020-01-15 14:52:08 +0200

    Update Utils.java

--- cde/core/util/Utils.java
+++ cde/core/util/Utils.java
@@ -5,6 +5,8 @@
 
 package cde.core.util;
 
+import cde.core.Defaults;
+import java.awt.Color;
 import net.minecraft.item.ItemStack;
 
 public class Utils
@@ -29,5 +31,51 @@ public class Utils
         
         
         return new double[] {min_u, max_u, min_v, max_v};
+    }
+    
+    public static int getClosestMinecraftColor(Color color)
+    {
+        int[] r = new int[16];
+        int[] g = new int[16];
+        int[] b = new int[16];
+        int[] d = new int[16];
+        
+        for(int i = 0; i < 16; i++)
+        {
+            r[i] = Defaults.MINECRAFT_COLORS[i].getRed() - color.getRed();
+            g[i] = Defaults.MINECRAFT_COLORS[i].getGreen() - color.getGreen();
+            b[i] = Defaults.MINECRAFT_COLORS[i].getBlue() - color.getBlue();
+            
+            if(r[i] < 0)
+            {
+                r[i] *= -1;
+            }
+            
+            if(g[i] < 0)
+            {
+                g[i] *= -1;
+            }
+            
+            if(b[i] < 0)
+            {
+                b[i] *= -1;
+            }
+            
+            d[i] = r[i] + g[i] + b[i];
+        }
+        
+        int distance = d[0];
+        int index = 0;
+
+        for(int i = 0; i < 16; i++)
+        {
+            if(d[i] < distance)
+            {
+                distance = d[i];
+                index = i;
+            }
+        }
+        
+        return index;
     }
 }
