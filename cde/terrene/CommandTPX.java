f66afd20412edc3d3beea304b0df3cfd74feee9d
StormTiberius <storm@tiberius>
2020-02-06 13:55:01 +0200

    Update CommandTPX.java

--- cde/terrene/CommandTPX.java
+++ cde/terrene/CommandTPX.java
@@ -5,7 +5,6 @@
 
 package cde.terrene;
 
-import cde.TerreneCore;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayerMP;
@@ -15,6 +14,18 @@ import net.minecraftforge.common.DimensionManager;
 
 public class CommandTPX extends CommandBase
 {
+    private final int OVERWORLD_ID,TROPICS_ID,EMBER_ID;
+    private final boolean TROPICS_ENABLED,EMBER_ENABLED;
+    
+    public CommandTPX(int tropicsId, int emberId, boolean tropicsEnabled, boolean emberEnabled)
+    {
+        OVERWORLD_ID = 0;
+        TROPICS_ID = tropicsId;
+        EMBER_ID = emberId;
+        TROPICS_ENABLED = tropicsEnabled;
+        EMBER_ENABLED = emberEnabled;
+    }
+    
     @Override
     public boolean canCommandSenderUseCommand(ICommandSender par1ICommandSender)
     {
@@ -42,14 +53,31 @@ public class CommandTPX extends CommandBase
             
             if(!player.worldObj.isRemote)
             {
-                int destination = TerreneCore.getDimensionId(TerreneCore.TROPICS);
+                int destination = OVERWORLD_ID;
                 
-                if(player.dimension == destination)
+                String origin = player.worldObj.provider.getDimensionName();
+                
+                if(origin.contentEquals("Tropics") && EMBER_ENABLED)
                 {
-                    destination = TerreneCore.getDimensionId(TerreneCore.EMBER);
+                    destination = EMBER_ID;
+                }
+                else if(origin.contentEquals("Ember") && TROPICS_ENABLED)
+                {
+                    destination = TROPICS_ID;
+                }
+                else if(origin.contentEquals("Overworld"))
+                {
+                    if(TROPICS_ENABLED)
+                    {
+                        destination = TROPICS_ID;
+                    }
+                    else if(EMBER_ENABLED)
+                    {
+                        destination = EMBER_ID;
+                    }
                 }
                 
-                if(DimensionManager.getWorld(destination) != null)
+                if(player.dimension != destination && DimensionManager.getWorld(destination) != null)
                 {
                     TeleporterTerrene.setDepartureCoords(player);
                     player.mcServer.getConfigurationManager().transferPlayerToDimension(player, destination, new TeleporterTerrene((WorldServer)player.worldObj, false));
