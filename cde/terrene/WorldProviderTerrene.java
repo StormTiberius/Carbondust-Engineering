d7989e9c51334e31e0bbffc7c971910182feb108
StormTiberius <storm@tiberius>
2020-02-10 20:56:10 +0200

    Update WorldProviderTerrene.java

--- cde/terrene/WorldProviderTerrene.java
+++ cde/terrene/WorldProviderTerrene.java
@@ -8,27 +8,25 @@ package cde.terrene;
 import cde.TerreneCore;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.ChunkCoordinates;
 import net.minecraft.world.WorldProviderSurface;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.storage.WorldInfo;
 
 public class WorldProviderTerrene extends WorldProviderSurface
-{    
+{
     private long time;
     
     @Override
     public String getDimensionName()
     {
-        return "Tropics";
+        return "Terrene";
     }
     
     @Override
     protected void registerWorldChunkManager()
     {
         worldChunkMgr = new WorldChunkManagerTerrene(worldObj);
-        dimensionId = TerreneCore.getDimensionId(TerreneCore.TROPICS);
     }
     
     @Override
@@ -42,28 +40,41 @@ public class WorldProviderTerrene extends WorldProviderSurface
     {
         int var4 = (int)(par1 % (24000L * TerreneCore.getDayCycleDurationMultiplier()));
         float var5 = ((float)var4 + par3) / (24000.0F * TerreneCore.getDayCycleDurationMultiplier()) - 0.25F;
-
+        
         if (var5 < 0.0F)
         {
             ++var5;
         }
-
+        
         if (var5 > 1.0F)
         {
             --var5;
         }
-
+        
         float var6 = var5;
         var5 = 1.0F - (float)((Math.cos((double)var5 * Math.PI) + 1.0D) / 2.0D);
         var5 = var6 + (var5 - var6) / 3.0F;
         return var5;
     }
-
+    
     @SideOnly(Side.CLIENT)
     @Override
     public int getMoonPhase(long par1, float par3)
     {
         return (int)(par1 / (24000L * TerreneCore.getDayCycleDurationMultiplier())) % 8;
+    }
+    
+    @SideOnly(Side.CLIENT)
+    @Override
+    public float getCloudHeight()
+    {
+        return 256.0F;
+    }
+    
+    @Override
+    public int getAverageGroundLevel()
+    {
+        return 128;
     }
     
     @SideOnly(Side.CLIENT)
@@ -81,14 +92,8 @@ public class WorldProviderTerrene extends WorldProviderSurface
     }
     
     @Override
-    public int getRespawnDimension(EntityPlayerMP player)
-    {
-        return dimensionId;
-    }
-
-    @Override
     public void updateWeather()
-    {   
+    {
         if(worldObj.isRemote)
         {
             super.updateWeather();
@@ -96,13 +101,13 @@ public class WorldProviderTerrene extends WorldProviderSurface
         else
         {
             WorldInfo worldInfo = worldObj.getWorldInfo();
-
+            
             time = worldInfo.getWorldTime();
-
+            
             if (!this.hasNoSky)
             {
                 int var1 = worldInfo.getThunderTime();
-
+                
                 if (var1 <= 0)
                 {
                     if (worldInfo.isThundering())
@@ -118,15 +123,15 @@ public class WorldProviderTerrene extends WorldProviderSurface
                 {
                     --var1;
                     worldInfo.setThunderTime(var1);
-
+                    
                     if (var1 <= 0)
                     {
                         worldInfo.setThundering(!worldInfo.isThundering());
                     }
                 }
-
+                
                 int var2 = worldInfo.getRainTime();
-
+                
                 if (var2 <= 0)
                 {
                     if (worldInfo.isRaining())
@@ -142,15 +147,15 @@ public class WorldProviderTerrene extends WorldProviderSurface
                 {
                     --var2;
                     worldInfo.setRainTime(var2);
-
+                    
                     if (var2 <= 0)
                     {
                         worldInfo.setRaining(!worldInfo.isRaining());
                     }
                 }
-
+                
                 worldObj.prevRainingStrength = worldObj.rainingStrength;
-
+                
                 if (worldInfo.isRaining())
                 {
                     worldObj.rainingStrength = (float)((double)worldObj.rainingStrength + 0.01D);
@@ -159,19 +164,19 @@ public class WorldProviderTerrene extends WorldProviderSurface
                 {
                     worldObj.rainingStrength = (float)((double)worldObj.rainingStrength - 0.01D);
                 }
-
+                
                 if (worldObj.rainingStrength < 0.0F)
                 {
                     worldObj.rainingStrength = 0.0F;
                 }
-
+                
                 if (worldObj.rainingStrength > 1.0F)
                 {
                     worldObj.rainingStrength = 1.0F;
                 }
-
+                
                 worldObj.prevThunderingStrength = worldObj.thunderingStrength;
-
+                
                 if (worldInfo.isThundering())
                 {
                     worldObj.thunderingStrength = (float)((double)worldObj.thunderingStrength + 0.01D);
@@ -180,12 +185,12 @@ public class WorldProviderTerrene extends WorldProviderSurface
                 {
                     worldObj.thunderingStrength = (float)((double)worldObj.thunderingStrength - 0.01D);
                 }
-
+                
                 if (worldObj.thunderingStrength < 0.0F)
                 {
                     worldObj.thunderingStrength = 0.0F;
                 }
-
+                
                 if (worldObj.thunderingStrength > 1.0F)
                 {
                     worldObj.thunderingStrength = 1.0F;
@@ -205,12 +210,30 @@ public class WorldProviderTerrene extends WorldProviderSurface
     }
     
     @Override
+    public int getHeight()
+    {
+        return 256;
+    }
+    
+    @Override
+    public int getActualHeight()
+    {
+        return 256;
+    }
+    
+    @Override
+    public double getHorizon()
+    {
+        return 127.0D;
+    }
+    
+    @Override
     public void resetRainAndThunder()
     {
         if(!worldObj.isRemote)
         {
             WorldInfo worldInfo = worldObj.getWorldInfo();
-
+            
             if(time != worldInfo.getWorldTime())
             {
                 long var2 = time + (24000L * TerreneCore.getDayCycleDurationMultiplier());
