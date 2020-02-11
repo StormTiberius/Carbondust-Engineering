9c78cf1ed4fd8aa2453ad4634489874019a310d5
StormTiberius <storm@tiberius>
2020-02-11 13:36:50 +0200

    Update EntityBatTerrene.java

--- cde/terrene/EntityBatTerrene.java
+++ cde/terrene/EntityBatTerrene.java
@@ -21,25 +21,33 @@ public class EntityBatTerrene extends EntityBat
     public boolean getCanSpawnHere()
     {
         int var1 = MathHelper.floor_double(this.boundingBox.minY);
-        int var2 = MathHelper.floor_double(this.posX);
-        int var3 = MathHelper.floor_double(this.posZ);
-        int var4 = this.worldObj.getBlockLightValue(var2, var1, var3);
-        byte var5 = 4;
-        Calendar var6 = this.worldObj.getCurrentDate();
 
-        if ((var6.get(2) + 1 != 10 || var6.get(5) < 20) && (var6.get(2) + 1 != 11 || var6.get(5) > 3))
+        if (var1 >= 113)
         {
-            if (this.rand.nextBoolean())
-            {
-                return false;
-            }
+            return false;
         }
         else
         {
-            var5 = 7;
-        }
+            int var2 = MathHelper.floor_double(this.posX);
+            int var3 = MathHelper.floor_double(this.posZ);
+            int var4 = this.worldObj.getBlockLightValue(var2, var1, var3);
+            byte var5 = 4;
+            Calendar var6 = this.worldObj.getCurrentDate();
 
-        return var4 > this.rand.nextInt(var5) ? false : getCanSpawnHereTerrene();
+            if ((var6.get(2) + 1 != 10 || var6.get(5) < 20) && (var6.get(2) + 1 != 11 || var6.get(5) > 3))
+            {
+                if (this.rand.nextBoolean())
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                var5 = 7;
+            }
+
+            return var4 > this.rand.nextInt(var5) ? false : getCanSpawnHereTerrene();
+        }
     }
     
     private boolean getCanSpawnHereTerrene()
