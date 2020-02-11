4493a7597c3d0d349ede2b99a8f35ffbe89b548b
StormTiberius <storm@tiberius>
2020-02-11 21:29:17 +0200

    Update WorldTypeEmber.java

--- cde/ember/WorldTypeEmber.java
+++ cde/ember/WorldTypeEmber.java
@@ -5,12 +5,40 @@
 
 package cde.ember;
 
+import cde.EmberCore;
+import net.minecraft.world.World;
 import net.minecraft.world.WorldType;
+import net.minecraft.world.biome.WorldChunkManager;
+import net.minecraft.world.chunk.IChunkProvider;
 
 public class WorldTypeEmber extends WorldType
 {
     public WorldTypeEmber(int id, String name, int version)
     {
         super(id,name,version);
+    }
+    
+    @Override
+    public WorldChunkManager getChunkManager(World world)
+    {
+        return new WorldChunkManagerEmber(EmberCore.ember, 0.8F, 0.4F);
+    }
+    
+    @Override
+    public IChunkProvider getChunkGenerator(World world, String generatorOptions)
+    {
+        return new ChunkProviderEmber(world, world.getSeed(), world.getWorldInfo().isMapFeaturesEnabled());
+    }
+    
+    @Override
+    public boolean hasVoidParticles(boolean var1)
+    {
+        return false;
+    }
+    
+    @Override
+    public double voidFadeMagnitude()
+    {
+        return 1.0D;
     }
 }
