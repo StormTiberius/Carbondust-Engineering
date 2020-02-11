a35a19a5a972eba588c77249559d00006548be4e
StormTiberius <storm@tiberius>
2020-02-11 22:26:45 +0200

    Update EmberCore.java

--- cde/EmberCore.java
+++ cde/EmberCore.java
@@ -11,6 +11,7 @@ import cde.ember.EmberEventManager;
 import cde.ember.EntityBatEmber;
 import cde.ember.EntitySquidEmber;
 import cde.ember.WorldProviderEmber;
+import cde.ember.WorldTypeEmber;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.PostInit;
@@ -25,6 +26,7 @@ import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.EntityRegistry;
 import java.io.File;
 import net.minecraft.block.Block;
+import net.minecraft.world.WorldType;
 import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraftforge.common.Configuration;
 import net.minecraftforge.common.DimensionManager;
@@ -39,9 +41,10 @@ public class EmberCore
 {
     private static Configuration cfg;
     private static boolean enabled,mobSpawnRules;
-    private static int biomeId,dimensionId,liquidId;
+    private static int biomeId,worldTypeId,liquidId;
     
-    public static BiomeGenBase ember;
+    public static BiomeGenBase biome;
+    public static WorldType ember;
     
     @PreInit
     public void preInit(FMLPreInitializationEvent event) 
@@ -53,13 +56,14 @@ public class EmberCore
         mobSpawnRules = cfg.get(Configuration.CATEGORY_GENERAL, "mobSpawnRules", true, "Mob Spawn Rules").getBoolean(true);
         
         biomeId = cfg.get(Configuration.CATEGORY_GENERAL, "emberBiomeId", 23, "Ember Biome Id").getInt();
-        dimensionId = cfg.get(Configuration.CATEGORY_GENERAL, "dimensionId", 2, "Ember Dimension Id").getInt();
+        worldTypeId = cfg.get(Configuration.CATEGORY_GENERAL, "worldTypeId", 9, "World Type Id").getInt();
         
         cfg.save();
         
         if(enabled)
         {
-            ember = (new BiomeGenEmber(biomeId)).setColor(16440917).setBiomeName("Ember").setDisableRain().setTemperatureRainfall(0.8F, 0.4F);
+            biome = (new BiomeGenEmber(biomeId)).setColor(16440917).setBiomeName("Ember").setDisableRain().setTemperatureRainfall(0.8F, 0.4F);
+            ember = new WorldTypeEmber(worldTypeId, "EMBER", 0);
             
             FMLInterModComms.sendMessage("CDE|Core", "add-oregen-for-world", "Ember");
         }
@@ -70,16 +74,8 @@ public class EmberCore
     {   
         if(enabled)
         {
-            if(dimensionId == 0 || dimensionId == -1 || dimensionId == 1)
-            {
-                DimensionManager.unregisterProviderType(dimensionId);
-                DimensionManager.registerProviderType(dimensionId, WorldProviderEmber.class, true);
-            }
-            else
-            {
-                DimensionManager.registerProviderType(dimensionId, WorldProviderEmber.class, true);
-                DimensionManager.registerDimension(dimensionId, dimensionId);
-            }
+            DimensionManager.unregisterProviderType(0);
+            DimensionManager.registerProviderType(0, WorldProviderEmber.class, true);
         
             EntityRegistry.registerModEntity(EntitySquidEmber.class, "EmberSquid", 0, this, 64, 3, true);
             EntityRegistry.registerModEntity(EntityBatEmber.class, "EmberBat", 1, this, 80, 3, false);
@@ -112,10 +108,5 @@ public class EmberCore
     public static int getLiquidId()
     {
         return liquidId;
-    }
-    
-    public static int getDimensionId()
-    {
-        return dimensionId;
     }
 }
