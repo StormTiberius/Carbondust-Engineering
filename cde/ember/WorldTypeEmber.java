a635ad83c1fbae9e762e0cd084b54362eaac9552
StormTiberius <storm@tiberius>
2020-02-11 21:11:44 +0200

    Create WorldTypeEmber.java

---
+++ cde/ember/WorldTypeEmber.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author StormTiberius
+ */
+
+package cde.ember;
+
+import net.minecraft.world.WorldType;
+
+public class WorldTypeEmber extends WorldType
+{
+    public WorldTypeEmber(int id, String name, int version)
+    {
+        super(id,name,version);
+    }
+}
