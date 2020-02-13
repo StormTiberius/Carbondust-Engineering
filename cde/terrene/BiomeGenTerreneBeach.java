782f751ac41d52b22a12439acca0b02309b530de
StormTiberius <storm@tiberius>
2020-02-13 14:29:33 +0200

    Revert "Rm"
    This reverts commit 39db7b41177b0fcdcee78522823a71f32e1d819d.

---
+++ cde/terrene/BiomeGenTerreneBeach.java
@@ -0,0 +1,29 @@
+/**
+ *
+ * @author StormTiberius
+ */
+
+package cde.terrene;
+
+import net.minecraft.block.Block;
+import net.minecraft.entity.passive.EntityOcelot;
+import net.minecraft.world.biome.SpawnListEntry;
+
+public class BiomeGenTerreneBeach extends BiomeGenTerrene
+{
+    public BiomeGenTerreneBeach(int id)
+    {
+        super(id);
+        
+        topBlock = (byte)Block.sand.blockID;
+        fillerBlock = (byte)Block.sand.blockID;
+        
+        spawnableCreatureList.clear();
+        spawnableMonsterList.add(new SpawnListEntry(EntityOcelot.class, 2, 1, 1));
+        spawnableWaterCreatureList.clear();
+        
+        theBiomeDecorator.treesPerChunk = 50;
+        theBiomeDecorator.reedsPerChunk = 50;
+        theBiomeDecorator.cactiPerChunk = 10;
+    }
+}
