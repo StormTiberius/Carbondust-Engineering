a69d7ca336636f00712ebb7cc8d38e57aab69a14
StormTiberius <storm@tiberius>
2020-02-14 15:12:01 +0200

    Update WorldGenSpawn.java

--- cde/terrene/WorldGenSpawn.java
+++ cde/terrene/WorldGenSpawn.java
@@ -25,17 +25,24 @@ public class WorldGenSpawn
     {
         ChunkCoordinates spawnPoint = WORLD.provider.getSpawnPoint();
         
-        for(int y = spawnPoint.posY - 1; y < spawnPoint.posX + 4; y++)
+        int xMin = spawnPoint.posX - 4;
+        int xMax = spawnPoint.posX + 5;
+        int yMin = spawnPoint.posY - 1;
+        int yMax = spawnPoint.posY + 3;
+        int zMin = spawnPoint.posZ - 4;
+        int zMax = spawnPoint.posZ + 5;
+        
+        for(int y = yMin; y <= yMax; y++)
         {
-            for(int x = spawnPoint.posX - 4; x < spawnPoint.posX + 6; x++)
+            for(int x = xMin; x <= xMax; x++)
             {
-                for(int z = spawnPoint.posZ - 4; z < spawnPoint.posZ + 6; z++)
+                for(int z = zMin; z <= zMax; z++)
                 {
-                    if(y == -1)
+                    if(y == yMin)
                     {
                         WORLD.setBlockWithNotify(x, y, z, Block.grass.blockID);
                     }
-                    else if(x == spawnPoint.posX - 4 || x == spawnPoint.posX + 6 || z == spawnPoint.posZ - 4 || z == spawnPoint.posZ + 6)
+                    else if(x == xMin || x == xMax || z == zMin || z == zMax)
                     {
                         WORLD.setBlockWithNotify(x, y, z, Block.cobblestone.blockID);
                     }
