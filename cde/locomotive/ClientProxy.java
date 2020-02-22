e124f00f881911da44115a813f2ffb8fb0c77de7
StormTiberius <storm@tiberius>
2020-02-22 16:13:46 +0200

    Update ClientProxy.java

--- cde/locomotive/ClientProxy.java
+++ cde/locomotive/ClientProxy.java
@@ -5,11 +5,15 @@
 
 package cde.locomotive;
 
+import cpw.mods.fml.client.registry.RenderingRegistry;
+import net.minecraftforge.client.MinecraftForgeClient;
+
 public class ClientProxy extends CommonProxy
 {
     @Override
     public void registerRenderers()
     {
-        
+        MinecraftForgeClient.preloadTexture("/cde/locomotive/locomotives.png");
+        RenderingRegistry.registerEntityRenderingHandler(EntityLocomotive.class, new RenderLocomotive());
     }
 }
