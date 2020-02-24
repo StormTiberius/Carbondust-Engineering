59ac5bba6f6e830ee67e0b9cd33033c6f14f7c35
StormTiberius <storm@tiberius>
2020-02-24 11:40:29 +0200

    Update PacketIds.java

--- cde/core/network/PacketIds.java
+++ cde/core/network/PacketIds.java
@@ -7,8 +7,8 @@ package cde.core.network;
 
 public class PacketIds
 {
-    public static final byte ENTITY = 0;
-    public static final byte TILE = 1;
-    public static final byte TILE_PARTICLE = 2;
-    public static final byte SOUND = 3;
+    public static final byte SOUND = 0;
+    public static final byte ENTITY = 1;
+    public static final byte TILE = 2;
+    public static final byte TILE_PARTICLE = 3;
 }
