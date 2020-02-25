b4b12237b724df2b824143b94ae4a848744f5fd2
StormTiberius <storm@tiberius>
2020-02-25 20:52:14 +0200

    Update ISoundSource.java

--- cde/api/ISoundSource.java
+++ cde/api/ISoundSource.java
@@ -13,11 +13,11 @@ public interface ISoundSource
     public abstract boolean isLooping();
     public abstract boolean isPlaying();
     public abstract void setPlaying(boolean flag);
+    public abstract int getOriginX();
+    public abstract int getOriginY();
+    public abstract int getOriginZ();
     public abstract int getAttModel();
     public abstract float getDistOrRoll();
-    public abstract float getOriginX();
-    public abstract float getOriginY();
-    public abstract float getOriginZ();
     public abstract float getVolume();
     public abstract float getPitch();
     public abstract String getSourceName();
