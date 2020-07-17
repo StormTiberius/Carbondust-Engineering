d845c93e45506d94760ab776e1c4e7c43cc78417
StormTiberius <me@co.fi>
2020-07-17 20:32:43 +0300

    Create Defaults.java

---
+++ cde/core/Defaults.java
@@ -0,0 +1,11 @@
+/**
+ *
+ * @author StormTiberius
+ */
+
+package cde.core;
+
+public class Defaults
+{
+    public static final int[] WEATHER_DURATIONS = {12000, 3600, 168000, 12000, 12000, 12000, 168000, 12000, 0, 0};
+}
