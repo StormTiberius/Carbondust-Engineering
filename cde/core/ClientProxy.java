238c1f4e44931c99c9ab13d41cf19c6dfdaf268f
StormTiberius <me@co.fi>
2020-07-17 19:42:45 +0300

    Create ClientProxy.java

---
+++ cde/core/ClientProxy.java
@@ -0,0 +1,41 @@
+/**
+ *
+ * @author StormTiberius
+ */
+
+package cde.core;
+
+import cde.sound.SoundEventManager;
+import cpw.mods.fml.client.FMLClientHandler;
+import net.minecraft.client.particle.EntityFX;
+import net.minecraft.network.packet.Packet;
+import net.minecraftforge.client.MinecraftForgeClient;
+import net.minecraftforge.common.MinecraftForge;
+
+public class ClientProxy extends CommonProxy
+{
+    @Override
+    public void preloadTextures()
+    {
+        MinecraftForgeClient.preloadTexture(Defaults.CDE_BLOCKS);
+        MinecraftForgeClient.preloadTexture(Defaults.CDE_ITEMS);
+    }
+    
+    @Override
+    public void sendToServer(Packet packet)
+    {
+        FMLClientHandler.instance().getClient().getSendQueue().addToSendQueue(packet);
+    }
+    
+    @Override
+    public void setupSound()
+    {
+        MinecraftForge.EVENT_BUS.register(new SoundEventManager());
+    }
+    
+    @Override
+    public void spawnParticle(EntityFX effect)
+    {
+        FMLClientHandler.instance().getClient().effectRenderer.addEffect(effect);
+    }
+}
