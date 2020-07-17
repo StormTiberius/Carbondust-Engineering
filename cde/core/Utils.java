89ff4732fca1266b8dcd1c9e46dadd36f0d406d3
StormTiberius <me@co.fi>
2020-07-17 21:37:47 +0300

    Update Utils.java

--- cde/core/Utils.java
+++ cde/core/Utils.java
@@ -9,9 +9,36 @@ import java.util.ArrayList;
 import java.util.List;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 public class Utils
 {
+    private static final double UV_SPACING = 1.0D / 16;
+    
+    public static double[] getUV(int index)
+    {
+        double u = index % 16;
+        double v = index / 16;
+        
+        double min_u = UV_SPACING * u;
+        double max_u = min_u + UV_SPACING;
+        
+        double min_v = UV_SPACING * v;
+        double max_v = min_v + UV_SPACING;
+        
+        return new double[] {min_u, max_u, min_v, max_v};
+    }
+    
+    public static ItemStack getNewItemStack(ItemStack is, int qty)
+    {
+        return new ItemStack(is.itemID, qty, is.getItemDamage());
+    }
+    
+    public static ItemStack getNewItemStack(ItemStack is)
+    {
+        return getNewItemStack(is, 1);
+    }
+    
     public static List<Block> getBlocksByClass(String name)
     {
         List<Block> blocks = new ArrayList();
