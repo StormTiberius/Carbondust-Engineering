4a2d13cf958f316cbaf46805cc82f24a8be2f323
StormTiberius <me@co.fi>
2020-07-17 18:52:16 +0300

    Create IPaintableTile.java

---
+++ cde/api/IPaintableTile.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author StormTiberius
+ */
+
+package cde.api;
+
+import java.awt.Color;
+
+public interface IPaintableTile
+{
+    public abstract boolean applyPaintCoating(Color color);
+}
