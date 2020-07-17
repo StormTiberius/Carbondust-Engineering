856cd9a8126878b2ea855da39a10a37633803d06
StormTiberius <me@co.fi>
2020-07-17 20:31:30 +0300

    Update ClientProxy.java

--- cde/core/ClientProxy.java
+++ cde/core/ClientProxy.java
@@ -17,8 +17,8 @@ public class ClientProxy extends CommonProxy
     @Override
     public void preloadTextures()
     {
-        MinecraftForgeClient.preloadTexture(Defaults.CDE_BLOCKS);
-        MinecraftForgeClient.preloadTexture(Defaults.CDE_ITEMS);
+        MinecraftForgeClient.preloadTexture(Info.CDE_BLOCKS);
+        MinecraftForgeClient.preloadTexture(Info.CDE_ITEMS);
     }
     
     @Override
