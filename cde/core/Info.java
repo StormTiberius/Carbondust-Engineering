90ba9dc030d6c52cfa234fffdedb7993e0e988b5
StormTiberius <me@co.fi>
2020-07-17 20:30:15 +0300

    Create Info.java

---
+++ cde/core/Info.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author StormTiberius
+ */
+
+package cde.core;
+
+public class Info
+{
+    public static final String VERSION = "2.0.0";
+    
+    public static final String CDE_BLOCKS = "/cde/blocks.png";
+    public static final String CDE_ITEMS = "/cde/items.png";
+}
