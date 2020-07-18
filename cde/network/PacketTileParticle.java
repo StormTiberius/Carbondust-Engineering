77ca21bc92cb80ce7d0c00c248e7aa664d0576ab
StormTiberius <me@co.fi>
2020-07-18 14:02:11 +0300

    Create PacketTileParticle.java

---
+++ cde/network/PacketTileParticle.java
@@ -0,0 +1,50 @@
+/**
+ *
+ * @author StormTiberius
+ */
+
+package cde.network;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import net.minecraft.tileentity.TileEntity;
+
+public class PacketTileParticle extends PacketTile
+{
+    public String particle;
+    public int count;
+    
+    public PacketTileParticle(){}
+    
+    public PacketTileParticle(TileEntity te, String particle, int count)
+    {
+        super(te);
+        this.particle = particle;
+        this.count = count;
+    }
+    
+    @Override
+    public byte getID()
+    {
+        return PacketIds.TILE_PARTICLE;
+    }
+    
+    @Override
+    public void writeData(DataOutputStream data) throws IOException
+    {
+        super.writeData(data);
+        
+        data.writeUTF(particle);
+        data.writeInt(count);
+    }
+    
+    @Override
+    public void readData(DataInputStream data) throws IOException
+    {
+        super.readData(data);
+        
+        particle = data.readUTF();
+        count = data.readInt();
+    }
+}
