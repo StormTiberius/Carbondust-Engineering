f15e6d24363878fc768967a4866ec706a1d677e0
StormTiberius <me@co.fi>
2020-07-18 22:28:15 +0300

    Create SoundSourceEvent.java

---
+++ cde/api/SoundSourceEvent.java
@@ -0,0 +1,33 @@
+/**
+ *
+ * @author StormTiberius
+ */
+
+package cde.api;
+
+import net.minecraft.world.World;
+import net.minecraftforge.event.world.WorldEvent;
+
+public class SoundSourceEvent extends WorldEvent
+{
+    public static enum Action
+    {
+        ADD,
+        REMOVE,
+        PLAY,
+        STOP,
+        MUTE,
+        UNMUTE,
+    }
+    
+    public ISoundSource iss;
+    public Action action;
+    
+    public SoundSourceEvent(World world, ISoundSource iss, Action action)
+    {
+        super(world);
+        
+        this.iss = iss;
+        this.action = action;
+    }
+}
