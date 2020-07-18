0d6369e84beff4203209196eda8927e21895a75e
StormTiberius <me@co.fi>
2020-07-18 13:13:52 +0300

    Create PacketIds.java

---
+++ cde/network/PacketIds.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author StormTiberius
+ */
+
+package cde.network;
+
+public class PacketIds
+{
+    public static final byte ENTITY = 0;
+    public static final byte TILE = 1;
+    public static final byte TILE_PARTICLE = 2;
+    public static final byte SOUND = 3;
+}
