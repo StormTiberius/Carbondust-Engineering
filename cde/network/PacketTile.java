3763c286cd64597d33d62f48fe8da749f19c6556
StormTiberius <me@co.fi>
2020-07-18 13:51:53 +0300

    Create PacketTile.java

---
+++ cde/network/PacketTile.java
@@ -0,0 +1,54 @@
+/**
+ *
+ * @author StormTiberius
+ */
+
+package cde.network;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import net.minecraft.tileentity.TileEntity;
+
+public class PacketTile extends PacketCde
+{
+    public int xCoord,yCoord,zCoord;
+    
+    public PacketTile(){}
+    
+    public PacketTile(int xCoord, int yCoord, int zCoord)
+    {
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
+        this.zCoord = zCoord;
+    }
+    
+    public PacketTile(TileEntity te)
+    {
+        this.xCoord = te.xCoord;
+        this.yCoord = te.yCoord;
+        this.zCoord = te.zCoord;
+    }
+    
+    @Override
+    public byte getID()
+    {
+        return PacketIds.TILE;
+    }
+    
+    @Override
+    public void writeData(DataOutputStream data) throws IOException
+    {
+        data.writeInt(xCoord);
+        data.writeInt(yCoord);
+        data.writeInt(zCoord);
+    }
+    
+    @Override
+    public void readData(DataInputStream data) throws IOException
+    {
+        xCoord = data.readInt();
+        yCoord = data.readInt();
+        zCoord = data.readInt();
+    }
+}
