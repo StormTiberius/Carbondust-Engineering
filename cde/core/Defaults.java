7708c672b771792dc0b4fd8cf8a6964a2b10800d
StormTiberius <me@co.fi>
2020-07-20 16:04:09 +0300

    Update Defaults.java

--- cde/core/Defaults.java
+++ cde/core/Defaults.java
@@ -7,8 +7,8 @@ package cde.core;
 
 public class Defaults
 {
-    public static final int BLOCK_ID_ORE = 180;
-    public static final int BLOCK_ID_STORAGE = 181;
+    public static final int BLOCK_ID_ORES = 180;
+    public static final int BLOCK_ID_RESOURCES = 181;
     public static final int BLOCK_ID_A = 182;
     public static final int BLOCK_ID_B = 183;
     public static final int BLOCK_ID_C = 184;
@@ -18,7 +18,7 @@ public class Defaults
     public static final int BLOCK_ID_G = 188;
     public static final int BLOCK_ID_H = 189;
     
-    public static final int ITEM_ID_MATERIAL = 500;
+    public static final int ITEM_ID_MATERIALS = 500;
     
     public static final int[] WEATHER_DURATIONS = {12000, 3600, 168000, 12000, 12000, 12000, 168000, 12000, 0, 0};
 }
