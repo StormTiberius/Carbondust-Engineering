bafac5a60509d62aa3e8d40943d54e9e4e48105e
StormTiberius <me@co.fi>
2020-07-20 23:29:49 +0300

    Update Materials.java

--- cde/api/Materials.java
+++ cde/api/Materials.java
@@ -9,5 +9,64 @@ import net.minecraft.item.ItemStack;
 
 public class Materials
 {
+    public static ItemStack ingotCopper;
+    public static ItemStack ingotTin;
+    public static ItemStack ingotSilver;
+    public static ItemStack ingotLead;
+    public static ItemStack ingotUranium;
+    public static ItemStack ingotZinc;
+    public static ItemStack ingotBronze;
+    public static ItemStack ingotBrass;
+    public static ItemStack ingotSteel;
     
+    public static ItemStack fuelPeat;
+    public static ItemStack fuelBituminousPeat;
+    
+    public static ItemStack dustCoal;
+    public static ItemStack dustCharcoal;
+    public static ItemStack dustIron;
+    public static ItemStack dustGold;
+    public static ItemStack dustCopper;
+    public static ItemStack dustTin;
+    public static ItemStack dustSilver;
+    public static ItemStack dustLead;
+    public static ItemStack dustZinc;
+    public static ItemStack dustBronze;
+    public static ItemStack dustBrass;
+    public static ItemStack dustSteel;
+    public static ItemStack dustSulfur;
+    public static ItemStack dustSaltpeter;
+    public static ItemStack dustQuartz;
+    public static ItemStack dustApatite;
+    
+    public static ItemStack nuggetIron;
+    public static ItemStack nuggetCopper;
+    public static ItemStack nuggetTin;
+    public static ItemStack nuggetSilver;
+    public static ItemStack nuggetLead;
+    public static ItemStack nuggetZinc;
+    public static ItemStack nuggetBronze;
+    public static ItemStack nuggetBrass;
+    public static ItemStack nuggetSteel;
+    
+    public static ItemStack gemQuartz;
+    public static ItemStack gemApatite;
+    public static ItemStack gemRuby;
+    public static ItemStack gemJade;
+    public static ItemStack gemSapphire;
+    
+    public static ItemStack fuelCoke;
+    
+    public static ItemStack plateIron;
+    public static ItemStack plateGold;
+    public static ItemStack plateCopper;
+    public static ItemStack plateTin;
+    public static ItemStack plateBronze;
+    public static ItemStack plateSteel;
+    
+    public static ItemStack circuitBoardSingle;
+    public static ItemStack circuitBoardDouble;
+    public static ItemStack circuitBoardMulti;
+    public static ItemStack electricMotor;
+    public static ItemStack electricWire;
 }
