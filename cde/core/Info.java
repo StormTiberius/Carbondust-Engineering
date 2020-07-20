cce389c50d2c29b0b1252bfa3c338d4d2362571f
StormTiberius <me@co.fi>
2020-07-21 00:27:08 +0300

    Update Info.java

--- cde/core/Info.java
+++ cde/core/Info.java
@@ -7,7 +7,19 @@ package cde.core;
 
 public class Info
 {
+    public static final String MOD_ID = "CDE";
+    
+    public static final String NAME = "CarbondustEngineering";
+    
     public static final String VERSION = "2.0.0";
+    
+    public static final String DEPENDENCIES = "required-after:Forge@[6.6.2.534,);"
+                                            + "required-after:IC2;"
+                                            + "required-after:BuildCraft|Core;"
+                                            + "required-after:Forestry;"
+                                            + "required-after:Railcraft;"
+                                            + "required-after:RedPowerCore;"
+                                            + "required-after:AppliedEnergistics";
     
     public static final String CDE_BLOCKS = "/cde/blocks.png";
     public static final String CDE_ITEMS = "/cde/items.png";
