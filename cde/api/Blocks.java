477ded1e46818ed74c02e435d8ad2b6b24022f49
StormTiberius <me@co.fi>
2020-07-20 21:14:23 +0300

    Update Blocks.java

--- cde/api/Blocks.java
+++ cde/api/Blocks.java
@@ -9,5 +9,29 @@ import net.minecraft.item.ItemStack;
 
 public class Blocks
 {
+    public static ItemStack oreCopper;
+    public static ItemStack oreTin;
+    public static ItemStack oreSilver;
+    public static ItemStack oreLead;
+    public static ItemStack oreUranium;
+    public static ItemStack oreSulfur;
+    public static ItemStack oreSaltpeter;
+    public static ItemStack oreQuartz;
+    public static ItemStack oreApatite;
+    public static ItemStack oreRuby;
+    public static ItemStack oreJade;
+    public static ItemStack oreSapphire;
     
+    public static ItemStack blockCopper;
+    public static ItemStack blockTin;
+    public static ItemStack blockSilver;
+    public static ItemStack blockLead;
+    public static ItemStack blockUranium;
+    public static ItemStack blockZinc;
+    public static ItemStack blockBronze;
+    public static ItemStack blockBrass;
+    public static ItemStack blockSteel;
+    public static ItemStack blockRuby;
+    public static ItemStack blockJade;
+    public static ItemStack blockSapphire;
 }
