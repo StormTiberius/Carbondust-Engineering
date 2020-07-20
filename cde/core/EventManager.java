9d99d12f42c91f843aaed97d850364e5deba0cb8
StormTiberius <me@co.fi>
2020-07-20 16:31:34 +0300

    Update EventManager.java

--- cde/core/EventManager.java
+++ cde/core/EventManager.java
@@ -38,7 +38,7 @@ public class EventManager
         if(event.action.equals(Action.RIGHT_CLICK_BLOCK))
         {
             ItemStack is = event.entityPlayer.getCurrentEquippedItem();
-
+            
             if(is != null)
             {
                 if(is.getDisplayName().equals("Fertilizer"))
@@ -53,19 +53,19 @@ public class EventManager
     }
     
     private boolean onFertilizerUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, Random itemRand, int par4, int par5, int par6)
-    {        
+    {
         int var11;
         int var12;
         int var13;
         
         var11 = par3World.getBlockId(par4, par5, par6);
-
+        
         BonemealEvent event = new BonemealEvent(par2EntityPlayer, par3World, var11, par4, par5, par6);
         if (MinecraftForge.EVENT_BUS.post(event))
         {
             return false;
         }
-
+        
         if (event.getResult() == Event.Result.ALLOW)
         {
             if (!par3World.isRemote)
@@ -74,7 +74,7 @@ public class EventManager
             }
             return true;
         }
-
+        
         if (var11 == Block.sapling.blockID)
         {
             if (!par3World.isRemote)
@@ -82,52 +82,52 @@ public class EventManager
                 ((BlockSapling)Block.sapling).growTree(par3World, par4, par5, par6, par3World.rand);
                 --par1ItemStack.stackSize;
             }
-
+            
             return true;
         }
-
+        
         if (var11 == Block.mushroomBrown.blockID || var11 == Block.mushroomRed.blockID)
         {
             if (!par3World.isRemote && ((BlockMushroom)Block.blocksList[var11]).fertilizeMushroom(par3World, par4, par5, par6, par3World.rand))
             {
                 --par1ItemStack.stackSize;
             }
-
+            
             return true;
         }
-
+        
         if (var11 == Block.melonStem.blockID || var11 == Block.pumpkinStem.blockID)
         {
             if (par3World.getBlockMetadata(par4, par5, par6) == 7)
             {
                 return false;
             }
-
+            
             if (!par3World.isRemote)
             {
                 ((BlockStem)Block.blocksList[var11]).fertilizeStem(par3World, par4, par5, par6);
                 --par1ItemStack.stackSize;
             }
-
+            
             return true;
         }
-
+        
         if (var11 > 0 && Block.blocksList[var11] instanceof BlockCrops)
         {
             if (par3World.getBlockMetadata(par4, par5, par6) == 7)
             {
                 return false;
             }
-
+            
             if (!par3World.isRemote)
             {
                 ((BlockCrops)Block.blocksList[var11]).fertilize(par3World, par4, par5, par6);
                 --par1ItemStack.stackSize;
             }
-
+            
             return true;
         }
-
+        
         if (var11 == Block.cocoaPlant.blockID)
         {
             if (!par3World.isRemote)
@@ -135,35 +135,35 @@ public class EventManager
                 par3World.setBlockMetadataWithNotify(par4, par5, par6, 8 | BlockDirectional.getDirection(par3World.getBlockMetadata(par4, par5, par6)));
                 --par1ItemStack.stackSize;
             }
-
+            
             return true;
         }
-
+        
         if (var11 == Block.grass.blockID)
         {
             if (!par3World.isRemote)
             {
                 --par1ItemStack.stackSize;
                 label133:
-
+                
                 for (var12 = 0; var12 < 128; ++var12)
                 {
                     var13 = par4;
                     int var14 = par5 + 1;
                     int var15 = par6;
-
+                    
                     for (int var16 = 0; var16 < var12 / 16; ++var16)
                     {
                         var13 += itemRand.nextInt(3) - 1;
                         var14 += (itemRand.nextInt(3) - 1) * itemRand.nextInt(3) / 2;
                         var15 += itemRand.nextInt(3) - 1;
-
+                        
                         if (par3World.getBlockId(var13, var14 - 1, var15) != Block.grass.blockID || par3World.isBlockNormalCube(var13, var14, var15))
                         {
                             continue label133;
                         }
                     }
-
+                    
                     if (par3World.getBlockId(var13, var14, var15) == 0)
                     {
                         if (itemRand.nextInt(10) != 0)
@@ -180,7 +180,7 @@ public class EventManager
                     }
                 }
             }
-
+            
             return true;
         }
         
