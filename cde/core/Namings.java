ab7dfe2b977f1090a5b3ec7fb27734eb017c1f55
StormTiberius <me@co.fi>
2020-07-21 13:30:53 +0300

    Update Namings.java

--- cde/core/Namings.java
+++ cde/core/Namings.java
@@ -7,5 +7,265 @@ package cde.core;
 
 public class Namings
 {
+        public static final String[] INTERNAL_PART_ITEM_NAMES =
+    {
+        "ingotCopper",
+        "ingotTin",
+        "ingotSilver",
+        "ingotLead",
+        "ingotUranium",
+        "ingotZinc",
+        "ingotBronze",
+        "ingotBrass",
+        "ingotSteel",
+        "fuelPeat",
+        "fuelBituminousPeat",
+        
+        "dustCoal",
+        "dustCharcoal",
+        "dustIron",
+        "dustGold",
+        "dustCopper",
+        "dustTin",
+        "dustSilver",
+        "dustLead",
+        "dustZinc",
+        "dustBronze",
+        "dustBrass",
+        "dustSteel",
+        "dustSulfur",
+        "dustSaltpeter",
+        "dustQuartz",
+        "dustApatite",
+        
+        "nuggetIron",
+        "nuggetCopper",
+        "nuggetTin",
+        "nuggetSilver",
+        "nuggetLead",
+        "nuggetZinc",
+        "nuggetBronze",
+        "nuggetBrass",
+        "nuggetSteel",
+        
+        "washedIron",
+        "washedGold",
+        "washedCopper",
+        "washedTin",
+        "washedSilver",
+        "washedLead",
+        "washedUranium",
+        
+        "crushedIron",
+        "crushedGold",
+        "crushedCopper",
+        "crushedTin",
+        "crushedSilver",
+        "crushedLead",
+        "crushedUranium",
+        
+        "gemQuartz",
+        "gemApatite",
+        "gemRuby",
+        "gemJade",
+        "gemSapphire",
+        
+        "fuelCoke",
+        
+        "plateIron",
+        "plateGold",
+        "plateCopper",
+        "plateTin",
+        "plateBronze",
+        "plateSteel",
+        
+        "circuitBoardSingle",
+        "circuitBoardDouble",
+        "circuitBoardMulti",
+        "electricMotor",
+        "electricWire"
+    };
     
+    public static final String[] EXTERNAL_PART_ITEM_NAMES =
+    {
+        "Copper Ingot",
+        "Tin Ingot",
+        "Silver Ingot",
+        "Lead Ingot",
+        "Uranium Ingot",
+        "Zinc Ingot",
+        "Bronze Ingot",
+        "Brass Ingot",
+        "Steel Ingot",
+        "Peat",
+        "Bituminous Peat",
+        
+        "Coal Dust",
+        "Charcoal Dust",
+        "Iron Dust",
+        "Gold Dust",
+        "Copper Dust",
+        "Tin Dust",
+        "Silver Dust",
+        "Lead Dust",
+        "Zinc Dust",
+        "Bronze Dust",
+        "Brass Dust",
+        "Steel Dust",
+        "Sulfur Dust",
+        "Saltpeter Dust",
+        "Quartz Dust",
+        "Fertilizer",
+
+        "Iron Nugget",
+        "Copper Nugget",
+        "Tin Nugget",
+        "Silver Nugget",
+        "Lead Nugget",
+        "Zinc Nugget",
+        "Bronze Nugget",
+        "Brass Nugget",
+        "Steel Nugget",
+
+        "Washed Iron Ore",
+        "Washed Gold Ore",
+        "Washed Copper Ore",
+        "Washed Tin Ore",
+        "Washed Silver Ore",
+        "Washed Lead Ore",
+        "Washed Uranium Ore",
+        
+        "Crushed Iron Ore",
+        "Crushed Gold Ore",
+        "Crushed Copper Ore",
+        "Crushed Tin Ore",
+        "Crushed Silver Ore",
+        "Crushed Lead Ore",
+        "Crushed Uranium Ore",
+        
+        "Quartz",
+        "Apatite",
+        "Ruby",
+        "Jade",
+        "Sapphire",
+       
+        "Coal Coke",
+        
+        "Iron Plate",
+        "Gold Plate",
+        "Copper Plate",
+        "Tin Plate",
+        "Bronze Plate",
+        "Steel Plate",
+        
+        "Single-Layer Circuit Board",
+        "Double-Layer Circuit Board",
+        "Multi-Layer Circuit Board",
+        "Electric Motor",
+        "Electric Wire"
+    };
+    
+    public static final String[] INTERNAL_ORE_BLOCK_NAMES =
+    {
+        "oreCopper",
+        "oreTin",
+        "oreSilver",
+        "oreLead",
+        "oreUranium",
+        "oreSulfur",
+        "oreSaltpeter",
+        "oreQuartz",
+        "oreApatite",
+        "oreRuby",
+        "oreJade",
+        "oreSapphire"
+    };
+        
+    public static final String[] EXTERNAL_ORE_BLOCK_NAMES =
+    {
+        "Copper Ore",
+        "Tin Ore",
+        "Silver Ore",
+        "Lead Ore",
+        "Uranium Ore",
+        "Sulfur Ore",
+        "Saltpeter Ore",
+        "Quartz Ore",
+        "Apatite Ore",
+        "Ruby Ore",
+        "Jade Ore",
+        "Sapphire Ore"
+    };
+    
+    public static final String[] INTERNAL_STORAGE_BLOCK_NAMES =
+    {
+        "blockCopper",
+        "blockTin",
+        "blockSilver",
+        "blockLead",
+        "blockUranium",
+        "blockZinc",
+        "blockBronze",
+        "blockBrass",
+        "blockSteel",
+        "blockRuby",
+        "blockJade",
+        "blockSapphire"
+    };
+        
+    public static final String[] EXTERNAL_STORAGE_BLOCK_NAMES =
+    {
+        "Block of Copper",
+        "Block of Tin",
+        "Block of Silver",
+        "Block of Lead",
+        "Block of Uranium",
+        "Block of Zinc",
+        "Block of Bronze",
+        "Block of Brass",
+        "Block of Steel",
+        "Block of Ruby",
+        "Block of Jade",
+        "Block of Sapphire"
+    };
+    
+    public static final String[] INTERNAL_BUILD_BLOCK_NAMES =
+    {
+        "blockConcrete"
+    };
+            
+    public static final String[] EXTERNAL_BUILD_BLOCK_NAMES =
+    {
+        "Block of Concrete"
+    };
+    
+    public static final String[] INTERNAL_MACHINE_ALPHA_BLOCK_NAMES =
+    {
+        "cdeMachineGenerator",
+        "cdeMachineTurbine",
+        "cdeMachineHeater",
+        "cdeMachinePump",
+        "cdeMachineMixer",
+        "cdeMachineSolarPanel",
+        "cdeMachineTransformer",
+        "cdeMachineSmokestack"
+    };
+    
+    public static final String[] EXTERNAL_MACHINE_ALPHA_BLOCK_NAMES =
+    {
+        "Generator",
+        "Turbine",
+        "Heater",
+        "Pump",
+        "Mixer",
+        "Solar Panel",
+        "Transformer",
+        "Smokestack"
+    };
+    
+    public static final String INTERNAL_GOGGLES_NAME = "cdeGoggles";
+    public static final String EXTERNAL_GOGGLES_NAME = "Nightvision Goggles";
+    
+    public static final String INTERNAL_GRATE_NAME = "cdeGrate";
+    public static final String EXTERNAL_GRATE_NAME= "Grate";
 }
