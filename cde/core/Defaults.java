35ea3b7a4fc38a3fcdb6acf9d4182ab6e4ea03ef
StormTiberius <me@co.fi>
2020-07-21 23:10:47 +0300

    Update Defaults.java

--- cde/core/Defaults.java
+++ cde/core/Defaults.java
@@ -7,6 +7,9 @@ package cde.core;
 
 public class Defaults
 {
+    public static final int[] WEATHER_DURATIONS = {12000, 3600, 168000, 12000, 12000, 12000, 168000, 12000, 0, 0};
+    
+    // BLOCK IDS
     public static final int BLOCK_ID_ORES = 180;
     public static final int BLOCK_ID_RESOURCES = 181;
     public static final int BLOCK_ID_A = 182;
@@ -18,7 +21,123 @@ public class Defaults
     public static final int BLOCK_ID_G = 188;
     public static final int BLOCK_ID_H = 189;
     
+    // ITEM IDS
     public static final int ITEM_ID_MATERIALS = 500;
     
-    public static final int[] WEATHER_DURATIONS = {12000, 3600, 168000, 12000, 12000, 12000, 168000, 12000, 0, 0};
+    // BLOCK TEXTURE INDEX
+    public static final int TEXTURE_ORE_RUBY = 0;
+    public static final int TEXTURE_ORE_JADE = 1;
+    public static final int TEXTURE_ORE_SAPPHIRE = 2;
+    public static final int TEXTURE_ORE_COPPER = 3;
+    public static final int TEXTURE_ORE_TIN = 4;
+    public static final int TEXTURE_ORE_SILVER = 5;
+    public static final int TEXTURE_ORE_LEAD = 6;
+    public static final int TEXTURE_ORE_URANIUM = 7;
+    public static final int TEXTURE_ORE_SULFUR = 8;
+    public static final int TEXTURE_ORE_SALTPETER = 9;
+    public static final int TEXTURE_ORE_APATITE = 10;
+    public static final int TEXTURE_ORE_QUARTZ = 11;
+    
+    public static final int TEXTURE_STORAGE_RUBY = 16;
+    public static final int TEXTURE_STORAGE_JADE = 17;
+    public static final int TEXTURE_STORAGE_SAPPHIRE = 18;
+    public static final int TEXTURE_STORAGE_COPPER = 19;
+    public static final int TEXTURE_STORAGE_TIN = 20;
+    public static final int TEXTURE_STORAGE_SILVER = 21;
+    public static final int TEXTURE_STORAGE_LEAD = 22;
+    public static final int TEXTURE_STORAGE_URANIUM = 23;
+    public static final int TEXTURE_STORAGE_ZINC = 24;
+    public static final int TEXTURE_STORAGE_BRONZE = 25;
+    public static final int TEXTURE_STORAGE_BRASS = 26;
+    public static final int TEXTURE_STORAGE_STEEL = 27;
+    
+    public static final int TEXTURE_MACHINE_SIDE = 32;
+    public static final int TEXTURE_MACHINE_BOTTOM = 33;
+    public static final int TEXTURE_MACHINE_CONNECTED = 34;
+    public static final int TEXTURE_MACHINE_GENERATOR_SIDE_OFF = 35;
+    public static final int TEXTURE_MACHINE_GENERATOR_SIDE_ON = 36;
+    public static final int TEXTURE_MACHINE_TURBINE_SIDE = 37;
+    public static final int TEXTURE_MACHINE_TURBINE_TOP_OFF = 38;
+    public static final int TEXTURE_MACHINE_TURBINE_TOP_ON = 39;
+    public static final int TEXTURE_MACHINE_HEATER_TOP = 40;
+    public static final int TEXTURE_MACHINE_PUMP_SIDE_OFF = 41;
+    public static final int TEXTURE_MACHINE_PUMP_SIDE_ON = 42;
+    public static final int TEXTURE_MACHINE_SOLAR_PANEL_TOP = 45;
+    public static final int TEXTURE_MACHINE_TRANSFORMER_TOP_OFF = 46;
+    public static final int TEXTURE_MACHINE_TRANSFORMER_TOP_ON = 47;
+    
+    public static final int TEXTURE_GRATE_BOTTOM_TOP = 49;
+    public static final int TEXTURE_GRATE_SIDE = 48;
+    public static final int TEXTURE_SPEAKER_BOTTOM_TOP = 50;
+    public static final int TEXTURE_SPEAKER_SIDE = 51;
+    public static final int TEXTURE_SMOKESTACK_BOTTOM = 56;
+    public static final int TEXTURE_SMOKESTACK_TOP = 57;
+    public static final int TEXTURE_SMOKESTACK_SIDE = 58;
+    
+    public static final int TEXTURE_DRUM_IRON_TOP = 54;
+    public static final int TEXTURE_DRUM_IRON_SIDE = 52;
+    public static final int TEXTURE_DRUM_STEEL_TOP = 55;
+    public static final int TEXTURE_DRUM_STEEL_SIDE = 53;
+    
+    // ITEM TEXTURE INDEX
+    public static final int TEXTURE_MATERIAL_INGOT_COPPER = 0;
+    public static final int TEXTURE_MATERIAL_INGOT_TIN = 1;
+    public static final int TEXTURE_MATERIAL_INGOT_SILVER = 2;
+    public static final int TEXTURE_MATERIAL_INGOT_LEAD = 3;
+    public static final int TEXTURE_MATERIAL_INGOT_URANIUM = 4;
+    public static final int TEXTURE_MATERIAL_INGOT_ZINC = 5;
+    public static final int TEXTURE_MATERIAL_INGOT_BRONZE = 6;
+    public static final int TEXTURE_MATERIAL_INGOT_BRASS = 7;
+    public static final int TEXTURE_MATERIAL_INGOT_STEEL = 8;
+    public static final int TEXTURE_MATERIAL_INGOT_PEAT = 9;
+    public static final int TEXTURE_MATERIAL_INGOT_BITUMINOUS_PEAT = 10;
+    
+    public static final int TEXTURE_MATERIAL_DUST_COAL = 16;
+    public static final int TEXTURE_MATERIAL_DUST_CHARCOAL = 17;
+    public static final int TEXTURE_MATERIAL_DUST_IRON = 18;
+    public static final int TEXTURE_MATERIAL_DUST_GOLD = 19;
+    public static final int TEXTURE_MATERIAL_DUST_COPPER = 20;
+    public static final int TEXTURE_MATERIAL_DUST_TIN = 21;
+    public static final int TEXTURE_MATERIAL_DUST_SILVER = 22;
+    public static final int TEXTURE_MATERIAL_DUST_LEAD = 23;
+    public static final int TEXTURE_MATERIAL_DUST_ZINC = 24;
+    public static final int TEXTURE_MATERIAL_DUST_BRONZE = 25;
+    public static final int TEXTURE_MATERIAL_DUST_BRASS = 26;
+    public static final int TEXTURE_MATERIAL_DUST_STEEL = 27;
+    public static final int TEXTURE_MATERIAL_DUST_SULFUR = 28;
+    public static final int TEXTURE_MATERIAL_DUST_SALTPETER = 29;
+    public static final int TEXTURE_MATERIAL_DUST_QUARTZ = 30;
+    public static final int TEXTURE_MATERIAL_DUST_APATITE = 31;
+    
+    public static final int TEXTURE_MATERIAL_NUGGET_IRON = 32;
+    public static final int TEXTURE_MATERIAL_NUGGET_COPPER = 33;
+    public static final int TEXTURE_MATERIAL_NUGGET_TIN = 34;
+    public static final int TEXTURE_MATERIAL_NUGGET_SILVER = 35;
+    public static final int TEXTURE_MATERIAL_NUGGET_LEAD = 36;
+    public static final int TEXTURE_MATERIAL_NUGGET_ZINC = 37;
+    public static final int TEXTURE_MATERIAL_NUGGET_BRONZE = 38;
+    public static final int TEXTURE_MATERIAL_NUGGET_BRASS = 39;
+    public static final int TEXTURE_MATERIAL_NUGGET_STEEL = 40;
+    
+    public static final int TEXTURE_MATERIAL_FUEL_COKE = 96;
+    public static final int TEXTURE_MATERIAL_FUEL_URANIUM = 96;
+    
+    public static final int TEXTURE_MATERIAL_GEM_QUARTZ = 83;
+    public static final int TEXTURE_MATERIAL_GEM_APATITE = 84;
+    public static final int TEXTURE_MATERIAL_GEM_RUBY = 80;
+    public static final int TEXTURE_MATERIAL_GEM_JADE = 81;
+    public static final int TEXTURE_MATERIAL_GEM_SAPPHIRE = 82;
+    
+    public static final int TEXTURE_MATERIAL_PLATE_IRON = 112;
+    public static final int TEXTURE_MATERIAL_PLATE_GOLD = 113;
+    public static final int TEXTURE_MATERIAL_PLATE_COPPER = 114;
+    public static final int TEXTURE_MATERIAL_PLATE_TIN = 115;
+    public static final int TEXTURE_MATERIAL_PLATE_BRONZE = 116;
+    public static final int TEXTURE_MATERIAL_PLATE_STEEL = 117;
+    
+    public static final int TEXTURE_MATERIAL_CIRCUIT_BOARD_SINGLE = 128;
+    public static final int TEXTURE_MATERIAL_CIRCUIT_BOARD_DOUBLE = 129;
+    public static final int TEXTURE_MATERIAL_CIRCUIT_BOARD_MULTI = 130;
+    public static final int TEXTURE_MATERIAL_ELECTRIC_MOTOR = 131;
+    public static final int TEXTURE_MATERIAL_ELECTRIC_WIRE = 132;
 }
