c614411f1b424c86cbe836d9601ea655f88271cc
StormTiberius <me@co.fi>
2020-07-24 21:47:33 +0300

    Update FuelManager.java

--- cde/manager/FuelManager.java
+++ cde/manager/FuelManager.java
@@ -5,6 +5,7 @@
 
 package cde.manager;
 
+import cde.core.Defaults;
 import cpw.mods.fml.common.IFuelHandler;
 import net.minecraft.item.ItemStack;
 
@@ -13,6 +14,16 @@ public class FuelManager implements IFuelHandler
     @Override
     public int getBurnTime(ItemStack fuel)
     {
+        if(fuel.itemID == ItemManager.materialItem.itemID)
+        {
+            switch(fuel.getItemDamage())
+            {
+                case 9: return Defaults.PEAT_FUEL_VALUE;
+                case 10: return Defaults.BITUMINOUS_PEAT_FUEL_VALUE;
+                case 36: return Defaults.COKE_FUEL_VALUE;
+            }
+        }
+        
         return 0;
     }
 }
